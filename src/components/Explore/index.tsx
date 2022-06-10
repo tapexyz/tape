04459ffff7271574e3241810247f245770a859b1
Sasi <me@sasi.codes>
2022-06-10 22:30:38 +0530

    feat: browse categories

--- src/components/Explore/index.tsx
+++ src/components/Explore/index.tsx
@@ -1,18 +1,22 @@
 import Layout from '@components/Common/Layout'
 import MetaTags from '@components/Common/MetaTags'
+import CategoriesShimmer from '@components/Shimmers/CategoriesShimmer'
 import dynamic from 'next/dynamic'
 
-// const Categories = dynamic(() => import('./Categories'))
+const Categories = dynamic(() => import('./Categories'), {
+  loading: () => <CategoriesShimmer />,
+  ssr: false
+})
 const Feed = dynamic(() => import('./Feed'))
 
 const Explore = () => {
   return (
     <Layout>
       <MetaTags title="Explore" />
-      {/* <Categories /> */}
-      {/* <div className="md:my-5"> */}
-      <Feed />
-      {/* </div> */}
+      <Categories />
+      <div className="md:my-5">
+        <Feed />
+      </div>
     </Layout>
   )
 }
---
+++ src/components/Shimmers/CategoryItemShimmer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryItemShimmer = () => {
+  return (
+    <div className="animate-pulse">
+      <div className="p-6 bg-gray-300 rounded-lg dark:bg-gray-700"></div>
+    </div>
+  )
+}
+
+export default CategoryItemShimmer
