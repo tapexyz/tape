aa682d3378290d60e49023cdc211e6ddc4c2a0c3
Sasi <me@sasi.codes>
2022-06-12 11:46:59 +0530

    fix: other channel overflow

--- src/components/Shimmers/VideoCardShimmer.tsx
+++ src/components/Shimmers/VideoCardShimmer.tsx
@@ -7,13 +7,13 @@ const VideoCardShimmer = () => {
         <div className="bg-gray-300 rounded-t-lg aspect-w-16 aspect-h-9 dark:bg-gray-700"></div>
         <div className="flex py-3 space-x-2">
           <div className="w-10 h-10 bg-gray-300 rounded-full dark:bg-gray-700"></div>
-          <div className="flex-1 py-1 space-y-4">
+          <div className="flex-1 py-1 space-y-2">
             <span className="space-y-2">
               <div className="h-2 bg-gray-300 rounded dark:bg-gray-700"></div>
               <div className="h-2 bg-gray-300 rounded dark:bg-gray-700"></div>
             </span>
             <div>
-              <div className="grid grid-cols-3 gap-1">
+              <div className="grid grid-cols-3">
                 <div className="h-2 col-span-2 bg-gray-300 rounded dark:bg-gray-700"></div>
               </div>
             </div>
