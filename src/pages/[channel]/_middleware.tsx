5b126a9bf37880715db79e6c441d8aa431aececd
Sasi <me@sasi.codes>
2022-06-16 20:22:00 +0530

    fix: handle

--- src/pages/[channel]/_middleware.tsx
+++ src/pages/[channel]/_middleware.tsx
@@ -1,4 +1,5 @@
 import { LENSTUBE_URL } from '@utils/constants'
+import { getHandle } from '@utils/functions/getHandle'
 import getProfilePicture from '@utils/functions/getProfilePicture'
 import { NextRequest } from 'next/server'
 import { Profile } from 'src/types'
@@ -15,25 +16,25 @@ export async function middleware(req: NextRequest) {
     const data = await result.json()
     const channel: Profile = data?.channel
     if (data?.success) {
-      const title = channel?.name || ''
-      const bio = channel.bio || 'Lenstube user'
+      const handle = `${getHandle(channel?.handle)} - Lenstube`
+      const bio = channel.bio || 'Lenstube channel'
       const pfp = getProfilePicture(channel)
       return new Response(
         `<!DOCTYPE html>
         <html lang="en">
             <head>
-            <title>${title}</title>
+            <title>${handle}</title>
             <meta name="description" content="${bio}" />
             <meta property="og:url" content=${LENSTUBE_URL} />
             <meta property="og:site_name" content="Lenstube" />
-            <meta property="og:title" content="${title}" />
+            <meta property="og:title" content="${handle}" />
             <meta property="og:description" content="${bio}" />
             <meta property="og:image" content="${pfp}" />
             <meta property="og:image:width" content="400" />
             <meta property="og:image:height" content="400" />
             <meta property="twitter:card" content="summary" />
             <meta property="twitter:site" content="Lenstube" />
-            <meta property="twitter:title" content="${title}" />
+            <meta property="twitter:title" content="${handle}" />
             <meta property="twitter:description" content="${bio}" />
             <meta property="twitter:image:src" content="${pfp}" />
             <meta property="twitter:image:width" content="400" />
