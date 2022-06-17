595820c85ad2dbfcf19a979bc36130c656027e17
Sasi <me@sasi.codes>
2022-06-17 21:15:38 +0530

    no data handling

--- src/components/Home/Feed.tsx
+++ src/components/Home/Feed.tsx
@@ -56,7 +56,12 @@ const HomeFeed = () => {
   })
 
   if (data?.timeline?.items?.length === 0) {
-    return <NoDataFound text="No videos yet." />
+    return (
+      <NoDataFound
+        withImage
+        text="You got no videos in your timeline, explore."
+      />
+    )
   }
 
   return (
--- src/components/UIElements/NoDataFound.tsx
+++ src/components/UIElements/NoDataFound.tsx
@@ -1,9 +1,19 @@
+import { STATIC_ASSETS } from '@utils/constants'
+import imageCdn from '@utils/functions/imageCdn'
 import React from 'react'
 
-export const NoDataFound = ({ text = 'No data found' }) => {
+export const NoDataFound = ({ text = 'No data found', withImage = false }) => {
   return (
-    <div className="p-1 space-y-1 rounded-lg">
-      <div className="text-sm font-medium">{text}</div>
+    <div className="flex flex-col items-center justify-center p-1 space-y-1 rounded-lg">
+      {withImage && (
+        <img
+          src={imageCdn(`${STATIC_ASSETS}/images/illustrations/empty.png`)}
+          className="w-32 my-4 md:w-36"
+          alt=""
+          draggable={false}
+        />
+      )}
+      <div className="text-sm font-medium text-center">{text}</div>
     </div>
   )
 }
