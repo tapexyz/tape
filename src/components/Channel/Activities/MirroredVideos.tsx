bae22754b401f3e340b8375eb4a7f1f1dacc5184
Sasi <me@sasi.codes>
2022-06-19 20:17:32 +0530

    fix: pub type

---
+++ src/components/Channel/Activities/MirroredVideos.tsx
@@ -0,0 +1,78 @@
+import { useQuery } from '@apollo/client'
+import TimelineShimmer from '@components/Shimmers/TimelineShimmer'
+import { Loader } from '@components/UIElements/Loader'
+import { NoDataFound } from '@components/UIElements/NoDataFound'
+import { LENSTUBE_APP_ID } from '@utils/constants'
+import { PROFILE_FEED_QUERY } from '@utils/gql/queries'
+import dynamic from 'next/dynamic'
+import React, { FC, useState } from 'react'
+import { useInView } from 'react-cool-inview'
+import { PaginatedResultInfo, Profile } from 'src/types'
+import { LenstubePublication } from 'src/types/local'
+
+const Timeline = dynamic(() => import('../../Home/Timeline'))
+
+type Props = {
+  channel: Profile
+}
+
+const MirroredVideos: FC<Props> = ({ channel }) => {
+  const [channelVideos, setChannelVideos] = useState<LenstubePublication[]>([])
+  const [pageInfo, setPageInfo] = useState<PaginatedResultInfo>()
+  const { data, loading, error, fetchMore } = useQuery(PROFILE_FEED_QUERY, {
+    variables: {
+      request: {
+        publicationTypes: 'MIRROR',
+        profileId: channel?.id,
+        limit: 8,
+        sources: [LENSTUBE_APP_ID]
+      }
+    },
+    skip: !channel?.id,
+    onCompleted(data) {
+      setPageInfo(data?.publications?.pageInfo)
+      setChannelVideos(data?.publications?.items)
+    }
+  })
+  const { observe } = useInView({
+    onEnter: () => {
+      fetchMore({
+        variables: {
+          request: {
+            publicationTypes: 'MIRROR',
+            profileId: channel?.id,
+            cursor: pageInfo?.next,
+            limit: 8,
+            sources: [LENSTUBE_APP_ID]
+          }
+        }
+      }).then(({ data }: any) => {
+        setPageInfo(data?.publications?.pageInfo)
+        setChannelVideos([...channelVideos, ...data?.publications?.items])
+      })
+    }
+  })
+
+  if (loading) return <TimelineShimmer />
+
+  if (data?.publications?.items?.length === 0) {
+    return <NoDataFound text="No comments on videos." />
+  }
+
+  return (
+    <div className="w-full">
+      {!error && !loading && (
+        <div>
+          <Timeline videos={channelVideos} videoType="Mirror" />
+          {pageInfo?.next && channelVideos.length !== pageInfo?.totalCount && (
+            <span ref={observe} className="flex justify-center p-10">
+              <Loader />
+            </span>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MirroredVideos
