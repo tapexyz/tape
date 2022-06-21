c62ec1c233e949c684a725f74833b1cabf6c00d1
Sasi <me@sasi.codes>
2022-06-21 10:21:49 +0530

    fix: header in mobile view

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
         'fixed z-10 flex md:left-[94px] right-2 left-2 md:right-4 items-center bg-white dark:bg-black h-14 md:h-16'
       )}
     >
-      <div className="flex w-3/4 md:justify-end">
+      <div className="flex justify-between flex-1 md:w-3/4 md:justify-end">
         <div className="flex items-center space-x-1.5 md:space-x-0">
           <Link href={HOME}>
             <a className="block md:hidden">
@@ -39,7 +39,7 @@ const Header = () => {
           <GlobalSearchBar />
         </div>
       </div>
-      <div className="flex flex-row items-center justify-end w-1/2 space-x-3">
+      <div className="flex flex-row items-center justify-end space-x-3 md:w-2/5">
         <NotificationTrigger />
         <NewVideoTrigger />
         <Login />
