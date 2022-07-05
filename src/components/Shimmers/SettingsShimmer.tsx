5e858e6c2f1e96ff027e9b3e8734c24b9d20195a
Sasi <me@sasi.codes>
2022-07-05 19:24:29 +0530

    loader

--- src/components/Shimmers/SettingsShimmer.tsx
+++ src/components/Shimmers/SettingsShimmer.tsx
@@ -1,18 +1,10 @@
+import { Loader } from '@components/UIElements/Loader'
 import React from 'react'
 
 const SettingsShimmer = () => {
   return (
-    <div className="w-full rounded-md">
-      <div className="flex flex-col md:space-x-4 animate-pulse">
-        <div className="grid gap-4 md:grid-cols-4">
-          <div className="col-span-4 md:col-span-1">
-            <div className="h-[80vh] bg-gray-200 rounded-md dark:bg-gray-800"></div>
-          </div>
-          <div className="col-span-4 md:col-span-3">
-            <div className="h-[80vh] bg-gray-200 rounded-md dark:bg-gray-800"></div>
-          </div>
-        </div>
-      </div>
+    <div className="grid h-[80vh] place-content-center">
+      <Loader />
     </div>
   )
 }
