970e5b66954455630a614b8501f823fa05aa90b7
Sasi <me@sasi.codes>
2022-07-13 20:00:29 +0530

    revert getFileFromDataURL

--- src/utils/functions/getFileFromDataURL.ts
+++ src/utils/functions/getFileFromDataURL.ts
@@ -1,6 +1,6 @@
 export const getFileFromDataURL = async (dataUrl: string, fileName: string) => {
   // convert base64 to raw binary data held in a string
-  let byteString = Buffer.from(dataUrl.split(',')[1], 'base64')
+  let byteString = atob(dataUrl.split(',')[1])
   // separate out the mime component
   let mimeString = dataUrl.split(',')[0].split(':')[1].split(';')[0]
   // write the bytes of the string to an ArrayBuffer
@@ -9,7 +9,7 @@ export const getFileFromDataURL = async (dataUrl: string, fileName: string) => {
   let ia = new Uint8Array(ab)
   // set the bytes of the buffer to the correct values
   for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString[i]
+    ia[i] = byteString.charCodeAt(i)
   }
   // write the ArrayBuffer to a blob, and you're done
   const blob = new Blob([ab], { type: mimeString })
