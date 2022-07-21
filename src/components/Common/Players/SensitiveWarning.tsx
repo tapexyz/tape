26c74fce75f6d2948aab2fc9161e6950e21298ee
Sasi <me@sasi.codes>
2022-07-21 20:50:50 +0530

    fix: bump dep

--- src/components/Common/Players/SensitiveWarning.tsx
+++ src/components/Common/Players/SensitiveWarning.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TbAlertTriangle } from 'react-icons/tb'
+import { IoWarningOutline } from 'react-icons/io5'
 
 type Props = {
   acceptWarning: () => void
@@ -7,19 +7,21 @@ type Props = {
 
 export default function SensitiveWarning({ acceptWarning }: Props) {
   return (
-    <div className="bg-black rounded-xl aspect-video flex w-full h-96 text-white justify-center items-center text-lg px-10 space-x-5">
-      <TbAlertTriangle className="h-28 w-28" />
+    <div className="flex items-center justify-center w-full px-10 space-x-5 text-lg text-white bg-black aspect-h-9 rounded-xl aspect-video h-96">
+      <IoWarningOutline className="h-28 w-28" />
       <div className="flex flex-col">
-        <div>
+        <div className="text-base">
           The following video has sensitive content and may inappropriate or
-          offensive to some audiences. Viewer discretion is advised
+          offensive to some audiences. <div>Viewer discretion is advised.</div>
         </div>
-        <button
-          className="bg-gray-800 px-5 py-2 mt-5 rounded w-80"
-          onClick={() => acceptWarning()}
-        >
-          I understand & wish to proceed
-        </button>
+        <div>
+          <button
+            className="px-5 py-2 mt-5 text-sm bg-gray-800 outline-none rounded-xl"
+            onClick={() => acceptWarning()}
+          >
+            I understand & wish to proceed
+          </button>
+        </div>
       </div>
     </div>
   )
