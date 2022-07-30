f68b2eff0edc461286034690170e175f944961d5
Ram <ramk97095@gmail.com>
2022-07-30 16:41:37 +0530

    fix:cursor removed

--- src/components/Home/FeedFilters.tsx
+++ src/components/Home/FeedFilters.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const FeedFilters = () => {
   return (
-    <div className="md:hidden flex space-x-2 cursor-pointer touch-pan-x overflow-x-scroll no-scrollbar mb-3">
+    <div className="md:hidden flex space-x-2 touch-pan-x overflow-x-scroll no-scrollbar mb-3">
       {filters.map((filter) => (
         <Link
           href={`/explore/${
@@ -12,7 +12,7 @@ const FeedFilters = () => {
           }`}
           key={filter.name}
         >
-          <div className="cursor rounded-full border border-gray-200 dark:border-gray-800 bg-gray-50 dark:bg-[#181818] px-3 py-1 transition duration-300 ease-in-out md:hover:scale-105">
+          <div className="rounded-full border border-gray-200 dark:border-gray-800 bg-gray-50 dark:bg-[#181818] px-3 py-1 transition duration-300 ease-in-out md:hover:scale-105">
             {filter.name}
           </div>
         </Link>
