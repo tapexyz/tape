ea01e9ba413361a4a67afe6aeb8ea59620c9aa8c
Sasi <me@sasi.codes>
2022-07-30 12:59:06 +0530

    udpate constants

--- src/utils/data/verified.ts
+++ src/utils/data/verified.ts
@@ -1,3 +1,5 @@
 import { IS_MAINNET } from '@utils/constants'
 
-export const VERIFIED_CHANNELS = IS_MAINNET ? [''] : ['0x2f', '0x03c7']
+export const VERIFIED_CHANNELS = IS_MAINNET
+  ? ['0x2d']
+  : ['0x2f', '0x03c7', '0x3675']
