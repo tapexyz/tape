ab691cdb9abdd4f1873f79c6fe202ea35d7c55f1
Sasi <me@sasi.codes>
2022-08-07 15:10:21 +0530

    fix: card shimmer

--- src/components/Shimmers/VideoDetailShimmer.tsx
+++ src/components/Shimmers/VideoDetailShimmer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useMemo } from 'react'
 
 import SuggestedShimmer from './SuggestedShimmer'
-import VideoCardShimmer from './VideoCardShimmer'
+import { CardShimmer } from './VideoCardShimmer'
 
 export const SuggestedVideosShimmer = () => {
   const cards = useMemo(() => Array(8).fill(1), [])
@@ -20,7 +20,7 @@ export const VideoDetailShimmer = () => {
   return (
     <div className="grid grid-cols-1 gap-y-4 md:gap-4 xl:grid-cols-4">
       <div className="col-span-3">
-        <VideoCardShimmer />
+        <CardShimmer />
       </div>
       <SuggestedVideosShimmer />
     </div>
