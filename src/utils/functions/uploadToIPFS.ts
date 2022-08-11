f604721d1fa5a1c39be07191bdcbb879bfbbd501
sasi.eth <me@sasi.codes>
2022-08-11 03:18:41 -0700

    chore: use infura (#330)

--- src/utils/functions/uploadToIPFS.ts
+++ src/utils/functions/uploadToIPFS.ts
@@ -2,24 +2,25 @@ import logger from '@lib/logger'
 import axios from 'axios'
 import { IPFSUploadResult } from 'src/types/local'
 
-const WEB3_STORAGE_TOKEN = process.env.NEXT_PUBLIC_WEB3_STORAGE_TOKEN
+const infuraSecret = process.env.NEXT_PUBLIC_INFURA_AUTHORIZATION_KEY as string
+const auth = Buffer.from(infuraSecret).toString('base64')
 
 const uploadImageToIPFS = async (file: File): Promise<IPFSUploadResult> => {
   try {
     const formData = new FormData()
     formData.append('file', file)
-    const uploaded = await axios('https://api.web3.storage/upload', {
+    const uploaded = await axios('https://ipfs.infura.io:5001/api/v0/add', {
       method: 'POST',
       data: formData,
       headers: {
         'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${WEB3_STORAGE_TOKEN}`
+        Authorization: `Basic ${auth}`
       }
     })
-    const { cid }: { cid: string } = await uploaded.data
+    const { Hash }: { Hash: string } = await uploaded.data
 
     return {
-      url: `ipfs://${cid}`,
+      url: `ipfs://${Hash}`,
       type: file.type || 'image/jpeg'
     }
   } catch (error) {
