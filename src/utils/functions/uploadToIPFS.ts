cdc5ca45fa6750eac576e96bff84e82027c0a465
sasi.eth <me@sasi.codes>
2022-08-16 05:57:55 -0700

    chore: replace infura with estuary (#352)

--- src/utils/functions/uploadToIPFS.ts
+++ src/utils/functions/uploadToIPFS.ts
@@ -2,25 +2,24 @@ import logger from '@lib/logger'
 import axios from 'axios'
 import { IPFSUploadResult } from 'src/types/local'
 
-const infuraSecret = process.env.NEXT_PUBLIC_INFURA_AUTHORIZATION_KEY as string
-const auth = Buffer.from(infuraSecret).toString('base64')
+const authKey = process.env.NEXT_PUBLIC_ESTUARY_AUTHORIZATION_KEY as string
 
 const uploadImageToIPFS = async (file: File): Promise<IPFSUploadResult> => {
   try {
     const formData = new FormData()
-    formData.append('file', file)
-    const uploaded = await axios('https://ipfs.infura.io:5001/api/v0/add', {
+    formData.append('data', file)
+    const uploaded = await axios('https://shuttle-4.estuary.tech/content/add', {
       method: 'POST',
       data: formData,
       headers: {
         'Content-Type': 'multipart/form-data',
-        Authorization: `Basic ${auth}`
+        Authorization: `Bearer ${authKey}`
       }
     })
-    const { Hash }: { Hash: string } = await uploaded.data
+    const { cid }: { cid: string } = await uploaded.data
 
     return {
-      url: `ipfs://${Hash}`,
+      url: `ipfs://${cid}`,
       type: file.type || 'image/jpeg'
     }
   } catch (error) {
