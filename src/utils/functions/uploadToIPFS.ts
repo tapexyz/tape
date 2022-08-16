6b4e0a17dbb0aa0f80546cab7e875e4a53b96873
sasi.eth <me@sasi.codes>
2022-08-16 06:18:37 -0700

    chore: shuffle array for endpoint (#353) (#354)

--- src/utils/functions/uploadToIPFS.ts
+++ src/utils/functions/uploadToIPFS.ts
@@ -2,20 +2,25 @@ import logger from '@lib/logger'
 import axios from 'axios'
 import { IPFSUploadResult } from 'src/types/local'
 
+import shuffleArray from './shuffleArray'
+
 const authKey = process.env.NEXT_PUBLIC_ESTUARY_AUTHORIZATION_KEY as string
 
 const uploadImageToIPFS = async (file: File): Promise<IPFSUploadResult> => {
   try {
     const formData = new FormData()
     formData.append('data', file)
-    const uploaded = await axios('https://shuttle-4.estuary.tech/content/add', {
-      method: 'POST',
-      data: formData,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${authKey}`
+    const uploaded = await axios(
+      `https://shuttle-${shuffleArray([4, 5, 6])}.estuary.tech/content/add`,
+      {
+        method: 'POST',
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${authKey}`
+        }
       }
-    })
+    )
     const { cid }: { cid: string } = await uploaded.data
 
     return {
