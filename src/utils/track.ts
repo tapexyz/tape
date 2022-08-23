dafaf09b4d413caf1fd36e911c9b9f4e6b7bceb2
Sasi <me@sasi.codes>
2022-08-23 22:37:05 +0530

    chore: add more events

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -1,5 +1,18 @@
+import mixpanel, { Dict } from 'mixpanel-browser'
+
+import { IS_MAINNET, MIXPANEL_TOKEN } from './constants'
+
 export const TRACK = {
   PAGE_VIEW: {
-    HOME: 'Home feed'
+    HOME: 'Home Page',
+    EXPLORE: 'Explore Page'
+  }
+}
+
+export const Mixpanel = {
+  track: (eventName: string, payload?: Dict) => {
+    if (MIXPANEL_TOKEN && IS_MAINNET) {
+      mixpanel.track(eventName, payload)
+    }
   }
 }
