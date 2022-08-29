fd47b670f691f52b9324d1d723a302ef7e525ae8
sasi.eth <me@sasi.codes>
2022-08-28 23:51:21 -0700

    chore: add remove sw snippet (#17)
    Co-Authored-By: bigint <69431456+bigint@users.noreply.github.com>
    
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

--- src/components/Common/Layout.tsx
+++ src/components/Common/Layout.tsx
@@ -121,6 +121,13 @@ const Layout: FC<Props> = ({ children }) => {
   }
 
   useEffect(() => {
+    // Remove service worker
+    // TODO: remove after a month
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister()
+      }
+    })
     validateAuthentication()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isDisconnected, address, chain, disconnect, selectedChannelId])
