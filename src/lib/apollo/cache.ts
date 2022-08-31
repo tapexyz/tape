7646dd3bbdb5136aa7d43513058d79abed196b5d
Sasi <me@sasi.codes>
2022-08-31 18:50:35 +0530

    chore: add possibleTypes

--- src/lib/apollo/cache.ts
+++ src/lib/apollo/cache.ts
@@ -1,5 +1,6 @@
 import { InMemoryCache } from '@apollo/client'
+import result from 'src/types'
 
-const cache = new InMemoryCache()
+const cache = new InMemoryCache({ possibleTypes: result.possibleTypes })
 
 export default cache
