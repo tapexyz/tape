1841e0ee7a25f3a28fda45b649185d2c2cda328a
Sasi <me@sasi.codes>
2022-09-06 16:26:30 +0530

    chore: validation error

--- src/components/Watch/NewComment.tsx
+++ src/components/Watch/NewComment.tsx
@@ -41,7 +41,7 @@ type Props = {
 }
 const formSchema = z.object({
   comment: z
-    .string()
+    .string({ required_error: 'Enter valid comment' })
     .trim()
     .min(1, { message: 'Enter valid comment' })
     .max(5000, { message: 'Comment should not exceed 5000 characters' })
