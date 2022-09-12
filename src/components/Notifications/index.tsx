a946b2f5f7de2167aed696745c72ff437ae4d0e1
Sasi <me@sasi.codes>
2022-09-12 20:11:05 +0530

    fix: notifications padding

--- src/components/Notifications/index.tsx
+++ src/components/Notifications/index.tsx
@@ -87,9 +87,7 @@ const Notifications = () => {
   return (
     <div className="mx-auto md:p-0 md:container md:max-w-2xl">
       <MetaTags title="Notifications" />
-      <h1 className="mb-4 text-lg font-medium md:mb-6 md:text-xl">
-        Notifications
-      </h1>
+      <h1 className="mb-4 text-lg font-medium">Notifications</h1>
       {loading && <NotificationsShimmer />}
       {notifications?.map((notification: Notification, index: number) => (
         <div
