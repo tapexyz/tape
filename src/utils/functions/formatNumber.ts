150b6c3beeea6684c482bb9a8b37245ff924c539
Sasi <me@sasi.codes>
2022-09-17 00:53:04 +0530

    update case

--- src/utils/functions/formatNumber.ts
+++ src/utils/functions/formatNumber.ts
@@ -1,8 +1,8 @@
 export const formatNumber = (num: number) => {
   if (num > 999 && num < 1000000) {
-    return (num / 1000).toFixed(0) + 'K'
+    return (num / 1000).toFixed(0) + 'k'
   } else if (num > 1000000) {
-    return (num / 1000000).toFixed(0) + 'M'
+    return (num / 1000000).toFixed(0) + 'm'
   } else if (num < 900) {
     return num
   }
