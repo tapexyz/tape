c39259c4dcb77767d06a05a6c0a22572b9ef1b79
sasi.eth <me@sasi.codes>
2022-09-21 16:11:05 +0530

    chore: sanitize lvpr url (#122)

--- src/utils/functions/getHlsData.ts
+++ src/utils/functions/getHlsData.ts
@@ -1,11 +1,15 @@
 import axios from 'axios'
+import { HLSData } from 'src/types/local'
+
+import sanitizeLvprUrl from './sanitizeLvprUrl'
 
 const getHlsData = async (playbackId: string) => {
   try {
     const { data } = await axios.get(
       `https://livepeer.studio/api/playback/${playbackId}`
     )
-    return data.meta?.source[0]
+    const hlsInfo: HLSData = data.meta?.source[0]
+    return { ...hlsInfo, url: sanitizeLvprUrl(hlsInfo.url) }
   } catch (error) {
     return null
   }
