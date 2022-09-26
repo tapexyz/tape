1a117dc9bbdce7bb3ac3887518c58d14db387646
Sasi <me@sasi.codes>
2022-09-26 19:33:03 +0530

    add console logs

--- src/utils/functions/getUserLocale.ts
+++ src/utils/functions/getUserLocale.ts
@@ -1,6 +1,12 @@
-const getUserLocale = () => 'en'
-// navigator.languages && navigator.languages?.length
-//   ? navigator.languages[0]
-//   : navigator.language
+import logger from '@lib/logger'
+
+const getUserLocale = () => {
+  const locale =
+    navigator.languages && navigator.languages?.length
+      ? navigator.languages[0]
+      : navigator.language
+  logger.log('[LOG => USER LOCALE]', locale)
+  return 'en'
+}
 
 export default getUserLocale
