f73eb7bd3b0d22a7200d864ec715bfb85a6d8b63
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2022-10-01 18:15:07 +0200

    Fix modal responsive

--- src/components/Common/Search/GlobalSearchBar.tsx
+++ src/components/Common/Search/GlobalSearchBar.tsx
@@ -62,86 +62,84 @@ const GlobalSearchBar: FC<Props> = ({ onSearchResults }) => {
   }
 
   return (
-    <div className="w-96">
-      <div ref={resultsRef}>
-        <div className="relative mt-1">
-          <div className="relative w-full overflow-hidden border border-gray-200 cursor-default dark:border-gray-800 rounded-xl sm:text-sm">
-            <input
-              className="w-full py-2 pl-3 pr-10 text-sm bg-transparent focus:outline-none"
-              onChange={(event) => setKeyword(event.target.value)}
-              placeholder="Search by hashtag / channel"
-              value={keyword}
+    <div ref={resultsRef}>
+      <div className="relative mt-1">
+        <div className="relative w-full overflow-hidden border border-gray-200 cursor-default dark:border-gray-800 rounded-xl sm:text-sm">
+          <input
+            className="w-full py-2 pl-3 pr-10 text-sm bg-transparent focus:outline-none"
+            onChange={(event) => setKeyword(event.target.value)}
+            placeholder="Search by hashtag / channel"
+            value={keyword}
+          />
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <AiOutlineSearch
+              className="w-5 h-5 text-gray-400"
+              aria-hidden="true"
             />
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <AiOutlineSearch
-                className="w-5 h-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </div>
           </div>
-          <div
-            className={clsx(
-              'md:absolute w-full mt-1 text-base bg-white overflow-hidden dark:bg-[#181818] rounded-xl ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
-              { hidden: debouncedValue.length === 0 }
-            )}
-          >
-            <Tab.Group>
-              <Tab.List className="flex justify-center">
-                <Tab
-                  className={({ selected }) =>
-                    clsx(
-                      'px-4 py-2 border-b-2 text-sm focus:outline-none w-full',
-                      selected
-                        ? 'border-indigo-500 opacity-100'
-                        : 'border-transparent opacity-50 hover:bg-indigo-500/[0.12]'
-                    )
-                  }
-                  onClick={() => {
-                    setActiveSearch('PUBLICATION')
-                  }}
-                >
-                  Videos
-                </Tab>
-                <Tab
-                  className={({ selected }) =>
-                    clsx(
-                      'px-4 py-2 border-b-2 text-sm focus:outline-none w-full',
-                      selected
-                        ? 'border-indigo-500 opacity-100'
-                        : 'border-transparent opacity-50 hover:bg-indigo-500/[0.12]'
-                    )
-                  }
-                  onClick={() => {
-                    setActiveSearch('PROFILE')
-                  }}
-                >
-                  Channels
-                </Tab>
-              </Tab.List>
-              <Tab.Panels>
-                <Tab.Panel className="overflow-y-auto max-h-[80vh] no-scrollbar focus:outline-none">
-                  <Videos
-                    results={channels?.search?.items}
-                    loading={loading}
-                    clearSearch={clearSearch}
-                  />
-                </Tab.Panel>
-                <Tab.Panel className="overflow-y-auto max-h-[80vh] no-scrollbar focus:outline-none">
-                  <Channels
-                    results={channels?.search?.items}
-                    loading={loading}
-                    clearSearch={clearSearch}
-                  />
-                </Tab.Panel>
-              </Tab.Panels>
-            </Tab.Group>
+        </div>
+        <div
+          className={clsx(
+            'md:absolute w-full mt-1 text-base bg-white overflow-hidden dark:bg-[#181818] rounded-xl ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
+            { hidden: debouncedValue.length === 0 }
+          )}
+        >
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              <Tab
+                className={({ selected }) =>
+                  clsx(
+                    'px-4 py-2 border-b-2 text-sm focus:outline-none w-full',
+                    selected
+                      ? 'border-indigo-500 opacity-100'
+                      : 'border-transparent opacity-50 hover:bg-indigo-500/[0.12]'
+                  )
+                }
+                onClick={() => {
+                  setActiveSearch('PUBLICATION')
+                }}
+              >
+                Videos
+              </Tab>
+              <Tab
+                className={({ selected }) =>
+                  clsx(
+                    'px-4 py-2 border-b-2 text-sm focus:outline-none w-full',
+                    selected
+                      ? 'border-indigo-500 opacity-100'
+                      : 'border-transparent opacity-50 hover:bg-indigo-500/[0.12]'
+                  )
+                }
+                onClick={() => {
+                  setActiveSearch('PROFILE')
+                }}
+              >
+                Channels
+              </Tab>
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel className="overflow-y-auto max-h-[80vh] no-scrollbar focus:outline-none">
+                <Videos
+                  results={channels?.search?.items}
+                  loading={loading}
+                  clearSearch={clearSearch}
+                />
+              </Tab.Panel>
+              <Tab.Panel className="overflow-y-auto max-h-[80vh] no-scrollbar focus:outline-none">
+                <Channels
+                  results={channels?.search?.items}
+                  loading={loading}
+                  clearSearch={clearSearch}
+                />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
 
-            {loading && (
-              <div className="flex justify-center p-5">
-                <Loader />
-              </div>
-            )}
-          </div>
+          {loading && (
+            <div className="flex justify-center p-5">
+              <Loader />
+            </div>
+          )}
         </div>
       </div>
     </div>
