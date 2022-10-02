64199f87d60c24247e8145d6c7f1dedbe4f24d37
Ujwal Kumar <ujwalkumar95@gmail.com>
2022-10-02 20:07:55 +0530

    Remove accept ownership checkbox from Details.tsx (#166)
    * Remove accept ownership checkbox from Details.tsx
    
    * Removed default values of accept terms in form

--- src/components/Upload/Details.tsx
+++ src/components/Upload/Details.tsx
@@ -38,10 +38,7 @@ const formSchema = z.object({
     .trim()
     .max(5000, { message: 'Description should not exceed 5000 characters' }),
   isSensitiveContent: z.boolean(),
-  disableComments: z.boolean(),
-  acceptTerms: z.boolean({
-    invalid_type_error: 'You must accept Terms'
-  })
+  disableComments: z.boolean()
 })
 
 export type VideoFormData = z.infer<typeof formSchema>
@@ -66,7 +63,6 @@ const Details: FC<Props> = ({ onUpload, onCancel }) => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       isSensitiveContent: uploadedVideo.isSensitiveContent ?? false,
-      acceptTerms: false,
       disableComments: uploadedVideo.disableComments ?? false,
       title: uploadedVideo.title,
       description: uploadedVideo.description
@@ -182,25 +178,6 @@ const Details: FC<Props> = ({ onUpload, onCancel }) => {
                 }
               />
             </div>
-          </div>
-          <div className="flex mt-6">
-            <input
-              id="default-checkbox"
-              type="checkbox"
-              checked={watch('acceptTerms')}
-              onChange={(e) => {
-                setValue('acceptTerms', e.target.checked)
-              }}
-              value=""
-              required
-              className="w-4 h-4 mt-[1px] text-indigo-600 bg-gray-100 border-gray-300 rounded-lg focus:outline-none dark:bg-gray-700 dark:border-gray-600"
-            />
-            <label
-              htmlFor="default-checkbox"
-              className="ml-2 text-sm opacity-80"
-            >
-              I own the content and it does not contain illegal information.
-            </label>
           </div>
         </div>
         <div className="flex flex-col items-start justify-between">
