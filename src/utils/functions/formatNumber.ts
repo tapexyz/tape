16dbe1204764a7439da3edcbd40c8b8366a6efa9
goverdhan07 <66107302+goverdhan07@users.noreply.github.com>
2022-10-05 22:04:53 +0530

    updated variable name and type

--- src/utils/functions/formatNumber.ts
+++ src/utils/functions/formatNumber.ts
@@ -1,9 +1,9 @@
 export const formatNumber = (num: number) => {
-var str = 'k'
+const attribute = 'k'
 if (num > 999 && num < 1000000) {
-    return `(num / 1000).toFixed(0) ${str}`
+    return `(num / 1000).toFixed(0) ${attribute}`
   } else if (num > 1000000) {
-    return `(num / 1000000).toFixed(0) ${str}`
+    return `(num / 1000000).toFixed(0) ${attribute}`
   } else if (num < 900) {
     return num
   }
