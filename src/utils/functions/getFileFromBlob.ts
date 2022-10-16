52f694183c4842fe13c5d07d25e7a83fd097a0aa
0xkeivin <keivincatwork@gmail.com>
2022-10-17 00:00:40 +0800

    added helper function

---
+++ src/utils/functions/getFileFromBlob.ts
@@ -0,0 +1,10 @@
+export const getFileFromBlob = (theBlob: Blob, fileName: string): File => {
+    return new File(
+        [theBlob as any], // cast as any
+        fileName,
+        {
+            lastModified: new Date().getTime(),
+            type: theBlob.type
+        }
+    )
+}
