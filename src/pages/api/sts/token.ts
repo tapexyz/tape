eb583be36c2c27d468016a4788aa9ce11d2660fb
Sasi <me@sasi.codes>
2022-10-29 16:30:36 +0530

    add mainnet validation

--- src/pages/api/sts/token.ts
+++ src/pages/api/sts/token.ts
@@ -4,6 +4,7 @@ import {
   API_ORIGINS,
   EVER_ACCESS_KEY,
   EVER_ACCESS_SECRET,
+  IS_MAINNET,
   NEXT_PUBLIC_EVER_TEMP_BUCKET_NAME
 } from '@utils/constants'
 import type { NextApiRequest, NextApiResponse } from 'next'
@@ -20,7 +21,7 @@ type Data = {
 
 const token = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const origin = req.headers.origin
-  if (!origin || !API_ORIGINS.includes(origin)) {
+  if (IS_MAINNET && (!origin || !API_ORIGINS.includes(origin))) {
     return res.status(403).json({ success: false })
   }
   if (req.method !== 'GET') return res.status(400).json({ success: false })
