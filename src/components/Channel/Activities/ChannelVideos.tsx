7f13ff56dfa17b6a730b357af43c3587def535e5
Sasi <me@sasi.codes>
2022-10-29 14:30:27 +0530

    chore: fetch videos

--- src/components/Channel/Activities/ChannelVideos.tsx
+++ src/components/Channel/Activities/ChannelVideos.tsx
@@ -36,6 +36,7 @@ const ChannelVideos: FC<Props> = ({ channel }) => {
     variables: {
       request
     },
+    fetchPolicy: 'no-cache',
     skip: !channel?.id,
     onCompleted(data) {
       setPageInfo(data?.publications?.pageInfo)
