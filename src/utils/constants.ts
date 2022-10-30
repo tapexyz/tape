369f76b16958b1ee8e00d81fe741e19191131be1
Sasi <me@sasi.codes>
2022-10-30 13:05:00 +0530

    fix: typo

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -70,8 +70,7 @@ export const BUNDLR_CURRENCY = 'matic'
 export const BUNDLR_WEBSITE_URL = 'https://bundlr.network'
 export const ARWEAVE_WEBSITE_URL = 'https://arweave.net'
 export const BUNDLR_PRIVATE_KEY = process.env.BUNDLR_PRIVATE_KEY as string
-export const BUNDLR_CONNECT_MESSAGE =
-  'Sign to initialize & estimating upload...'
+export const BUNDLR_CONNECT_MESSAGE = 'Sign to initialize & estimate upload...'
 
 // error messages
 export const ERROR_MESSAGE = 'Oops, something went something!'
