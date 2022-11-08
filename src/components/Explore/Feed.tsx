49e1cb7139c38466f9736d619ac5ee3f93250e2b
sasi.eth <me@sasi.codes>
2022-11-08 13:04:13 +0530

    chore: remove recents (#298)

--- src/components/Explore/Feed.tsx
+++ src/components/Explore/Feed.tsx
@@ -1,4 +1,6 @@
 import { Tab } from '@headlessui/react'
+import useAppStore from '@lib/store'
+import { ADMIN_IDS } from '@utils/constants'
 import clsx from 'clsx'
 import React from 'react'
 import { AiOutlineFire } from 'react-icons/ai'
@@ -10,6 +12,9 @@ import Recents from './Recents'
 import Trending from './Trending'
 
 const ExploreFeed = () => {
+  const selectedChannel = useAppStore((state) => state.selectedChannel)
+  const isAdmin = ADMIN_IDS.includes(selectedChannel?.id)
+
   return (
     <div>
       <div className="w-full col-span-9">
@@ -41,19 +46,21 @@ const ExploreFeed = () => {
               <AiOutlineFire />
               <span>Trending</span>
             </Tab>
-            <Tab
-              className={({ selected }) =>
-                clsx(
-                  'px-4 py-2 flex items-center space-x-2 border-b-2 text-sm focus:outline-none',
-                  selected
-                    ? 'border-indigo-900 opacity-100'
-                    : 'border-transparent opacity-50'
-                )
-              }
-            >
-              <BiTimeFive />
-              <span>Recents</span>
-            </Tab>
+            {isAdmin && (
+              <Tab
+                className={({ selected }) =>
+                  clsx(
+                    'px-4 py-2 flex items-center space-x-2 border-b-2 text-sm focus:outline-none',
+                    selected
+                      ? 'border-indigo-900 opacity-100'
+                      : 'border-transparent opacity-50'
+                  )
+                }
+              >
+                <BiTimeFive />
+                <span>Recents</span>
+              </Tab>
+            )}
           </Tab.List>
           <Tab.Panels>
             <Tab.Panel className="py-3 focus:outline-none">
@@ -62,9 +69,11 @@ const ExploreFeed = () => {
             <Tab.Panel className="py-3 focus:outline-none">
               <Trending />
             </Tab.Panel>
-            <Tab.Panel className="py-3 focus:outline-none">
-              <Recents />
-            </Tab.Panel>
+            {isAdmin && (
+              <Tab.Panel className="py-3 focus:outline-none">
+                <Recents />
+              </Tab.Panel>
+            )}
           </Tab.Panels>
         </Tab.Group>
       </div>
