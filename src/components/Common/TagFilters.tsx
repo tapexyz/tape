210b8b1a9d2fec745876a78d07b33d6e75cc59df
Sasi <me@sasi.codes>
2022-11-16 22:07:01 +0530

    chore: tags scroll

--- src/components/Common/TagFilters.tsx
+++ src/components/Common/TagFilters.tsx
@@ -1,14 +1,19 @@
 import useAppStore from '@lib/store'
 import { CREATOR_VIDEO_CATEGORIES } from '@utils/data/categories'
+import useHorizontalScroll from '@utils/hooks/useHorizantalScroll'
 import clsx from 'clsx'
 import React from 'react'
 
 const TagFilters = () => {
   const activeTagFilter = useAppStore((state) => state.activeTagFilter)
   const setActiveTagFilter = useAppStore((state) => state.setActiveTagFilter)
+  const scrollRef = useHorizontalScroll()
 
   return (
-    <div className="flex px-2 scroll-smooth overflow-x-auto touch-pan-x no-scrollbar pt-4 space-x-2 ultrawide:max-w-[110rem] mx-auto">
+    <div
+      ref={scrollRef}
+      className="flex px-2 scroll-smooth overflow-x-auto touch-pan-x no-scrollbar pt-4 space-x-2 ultrawide:max-w-[110rem] mx-auto"
+    >
       <button
         type="button"
         onClick={() => setActiveTagFilter('all')}
