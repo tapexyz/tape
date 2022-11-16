c3f2b75cb229f45a7c2397fab4ab79ade5b8bb88
Sasi <me@sasi.codes>
2022-11-16 23:03:34 +0530

    exp: ssr

--- src/pages/watch/[id].tsx
+++ src/pages/watch/[id].tsx
@@ -1,3 +1,23 @@
 import VideoDetails from '@components/Watch'
+import type { GetServerSideProps } from 'next'
+import { UAParser } from 'ua-parser-js'
 
 export default VideoDetails
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const parser = new UAParser()
+  const os = parser.getOS()
+  const pubId = context.params?.id
+  if (!os.name && pubId) {
+    context.res.setHeader('Cache-Control', 'public, s-maxage=86400')
+    return {
+      redirect: {
+        permanent: false,
+        destination: `https://embed.lenstube.xyz/${pubId}`
+      }
+    }
+  }
+  return {
+    props: {}
+  }
+}
