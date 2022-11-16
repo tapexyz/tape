5ba4054ce302abba6796fcd48b33fece34a4adde
Sasi <me@sasi.codes>
2022-11-17 00:30:47 +0530

    remove ssr

--- src/pages/watch/[id].tsx
+++ src/pages/watch/[id].tsx
@@ -1,22 +1,3 @@
 import VideoDetails from '@components/Watch'
-import type { GetServerSideProps } from 'next'
-import parser from 'ua-parser-js'
 
 export default VideoDetails
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const ua = parser(context.req?.headers['user-agent'])
-  const pubId = context.params?.id
-  if (!ua.os.name && pubId) {
-    context.res.setHeader('Cache-Control', 'public, s-maxage=86400')
-    return {
-      redirect: {
-        permanent: false,
-        destination: `https://embed.lenstube.xyz/${pubId}`
-      }
-    }
-  }
-  return {
-    props: {}
-  }
-}
