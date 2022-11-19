58b81bddb666ba207fd0cbd68cb559fac45502a0
Sasi <me@sasi.codes>
2022-11-19 18:06:31 +0530

    chore: cleanup

--- apps/badge/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from './server'
-
-const port = process.env.PORT || 5001
-const server = createServer()
-
-server.listen(port, () => {
-  console.log(`API running on http://localhost:${port}`)
-})
