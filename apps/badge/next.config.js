fac3830091c43aa83c04a46bf7d8d0278ca8f0eb
Sasi <me@sasi.codes>
2022-11-22 14:04:57 +0530

    chore: remove badge app

--- apps/badge/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')(['utils'])
-
-const nextConfig = withTM({
-  reactStrictMode: true
-})
-
-module.exports = nextConfig
