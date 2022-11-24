4e235018e6c8c985f1f2afa7c0f764e29f450b75
Sasi <me@sasi.codes>
2022-11-24 13:59:08 +0530

    fix: meta values

--- packages/utils/functions/getMetaTags.tsx
+++ packages/utils/functions/getMetaTags.tsx
@@ -35,15 +35,15 @@ const getMetaTags = ({
   }
 
   let defaultMeta = `<title>${meta.title}</title>
-              <meta content=${meta.description} name="description" />
+              <meta content="${meta.description}" name="description" />
               <meta name="robots" content="follow, index" />
               <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-              <link rel="canonical" href=${meta.url} />
-              <meta property="og:url" content=${meta.url} />
-              <meta property="og:site_name" content=${LENSTUBE_APP_NAME} />
-              <meta property="og:description" content=${meta.description} />
-              <meta property="og:title" content=${meta.title} />
-              <meta property="og:image" content=${meta.image} />
+              <link rel="canonical" href="${meta.url}" />
+              <meta property="og:url" content="${meta.url}" />
+              <meta property="og:site_name" content="${LENSTUBE_APP_NAME}" />
+              <meta property="og:description" content="${meta.description}" />
+              <meta property="og:title" content="${meta.title}" />
+              <meta property="og:image" content="${meta.image}" />
               <meta property="og:image:width" content="${
                 isVideo ? 480 : 400
               }" />
@@ -57,27 +57,27 @@ const getMetaTags = ({
                 isVideo ? 360 : 400
               }" />
               <meta name="twitter:site" content="@lenstubexyz" />
-              <meta name="twitter:title" content=${meta.title} />
-              <meta name="twitter:description" content=${meta.description} />
-              <meta property="twitter:image" content=${meta.image} />
-              <meta property="twitter:creator" content=${LENSTUBE_TWITTER_HANDLE} />
+              <meta name="twitter:title" content="${meta.title}" />
+              <meta name="twitter:description" content="${meta.description}" />
+              <meta property="twitter:image" content="${meta.image}" />
+              <meta property="twitter:creator" content="${LENSTUBE_TWITTER_HANDLE}" />
               <meta name="twitter:card" content=${
                 isVideo ? 'summary_large_image' : 'summary'
               } />`
 
   if (isVideo) {
     const embedUrl = `${LENSTUBE_EMBED_URL}/${pubId}`
-    defaultMeta += `<meta property="og:video" content=${LENSTUBE_EMBED_URL} />
+    defaultMeta += `<meta property="og:video" content="${LENSTUBE_EMBED_URL}" />
       <meta property="og:video:width" content="1280" />
       <meta property="og:video:height" content="720" />
-      <meta property="og:video:url" content=${meta.url} />
+      <meta property="og:video:url" content="${meta.url}" />
       <meta property="og:video:type" content="text/html" />
-      <meta property="og:video:secure_url" content=${meta.url}/>
+      <meta property="og:video:secure_url" content="${meta.url}"/>
       <meta name="twitter:card" content="player" />
-      <meta name="twitter:player" content=${embedUrl} />
+      <meta name="twitter:player" content="${embedUrl}" />
       <meta property="twitter:player:width" content="1280" />
       <meta property="twitter:player:height" content="720" />
-      <link rel="iframely player" type="text/html" href=${embedUrl} media="(aspect-ratio: 1280/720)" />`
+      <link rel="iframely player" type="text/html" href="${embedUrl}" media="(aspect-ratio: 1280/720)" />`
   }
 
   return `<!DOCTYPE html>
