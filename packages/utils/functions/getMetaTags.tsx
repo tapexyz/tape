62f49857bc69f608f63855e05d18ca1ac32f8164
Sasi <me@sasi.codes>
2022-11-25 18:31:41 +0530

    chore: update metatags

--- packages/utils/functions/getMetaTags.tsx
+++ packages/utils/functions/getMetaTags.tsx
@@ -70,6 +70,7 @@ const getMetaTags = ({
     defaultMeta += `<meta property="og:video" content="${LENSTUBE_EMBED_URL}" />
       <meta property="og:video:width" content="1280" />
       <meta property="og:video:height" content="720" />
+      <meta property="og:video" content="${embedUrl}" />
       <meta property="og:video:url" content="${embedUrl}" />
       <meta property="og:video:type" content="text/html" />
       <meta property="og:video:secure_url" content="${meta.url}"/>
