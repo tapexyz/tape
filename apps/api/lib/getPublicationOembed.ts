5c5cff171092fbcf0131fdeb4f447a6585d6d21b
Sasi <me@sasi.codes>
2022-12-01 00:20:04 +0530

    chore: cleanup

--- apps/api/lib/getPublicationOembed.ts
+++ apps/api/lib/getPublicationOembed.ts
@@ -1,6 +1,11 @@
 import { PublicationDetailsDocument } from 'lens'
 import type { LenstubePublication } from 'utils'
-import { STATIC_ASSETS } from 'utils'
+import {
+  LENSTUBE_APP_NAME,
+  LENSTUBE_EMBED_URL,
+  LENSTUBE_WEBSITE_URL,
+  STATIC_ASSETS
+} from 'utils'
 import getApolloClient from 'utils/functions/getApolloClient'
 import getThumbnailUrl from 'utils/functions/getThumbnailUrl'
 import imageCdn from 'utils/functions/imageCdn'
@@ -26,34 +31,34 @@ const getPublicationOembed = async (publicationId: string, format: string) => {
       return {
         title: title,
         author_name: video.profile?.handle,
-        author_url: `https://lenstube.xyz/channel/${video.profile?.handle}`,
+        author_url: `${LENSTUBE_WEBSITE_URL}/channel/${video.profile?.handle}`,
         type: 'video',
         height: 113,
         width: 200,
         version: '1.0',
-        provider_name: 'Lenstube',
-        provider_url: 'https://lenstube.xyz/',
+        provider_name: LENSTUBE_APP_NAME,
+        provider_url: LENSTUBE_WEBSITE_URL,
         thumbnail_height: 360,
         thumbnail_width: 480,
         thumbnail_url: thumbnail,
-        html: `<iframe width=\"200\" height=\"113\" src=\"https://embed.lenstube.xyz/${video.id}\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen title=\"${title}\"></iframe>`
+        html: `<iframe width=\"200\" height=\"113\" src=\"${LENSTUBE_EMBED_URL}/${video.id}\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen title=\"${title}\"></iframe>`
       }
     }
     if (format === 'xml') {
       return `<oembed>
               <title>${title}</title>
               <author_name>${video.profile?.handle}</author_name>
-              <author_url>https://lenstube.xyz/channel/${video.profile?.handle}</author_url>
+              <author_url>${LENSTUBE_WEBSITE_URL}/channel/${video.profile?.handle}</author_url>
               <type>video</type>
               <height>113</height>
               <width>200</width>
               <version>1.0</version>
-              <provider_name>Lenstube</provider_name>
-              <provider_url>https://lenstube.xyz/</provider_url>
+              <provider_name>${LENSTUBE_APP_NAME}</provider_name>
+              <provider_url>${LENSTUBE_WEBSITE_URL}</provider_url>
               <thumbnail_height>360</thumbnail_height>
               <thumbnail_width>480</thumbnail_width>
               <thumbnail_url>${thumbnail}</thumbnail_url>
-              <html><iframe width="200" height="113" src="https://embed.lenstube.xyz/${video.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="${title}"></iframe></html>
+              <html><iframe width="200" height="113" src="${LENSTUBE_EMBED_URL}/${video.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="${title}"></iframe></html>
               </oembed>`
     }
   } catch {
