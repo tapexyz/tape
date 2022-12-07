452b43708ca10f5a335fcad9d6c0be8aa0883c76
Sasi <me@sasi.codes>
2022-12-07 21:13:24 +0530

    chore: remove axiom

--- packages/utils/logger.ts
+++ packages/utils/logger.ts
@@ -1,16 +1,12 @@
-import { log } from 'next-axiom'
-
 const logger = {
   log: (message: string, info: any) => {
     console.log(message, info)
-    log.info(message, info)
   },
   warn: (...args: any) => {
     console.warn(...args)
   },
   error: (message: string, error: any) => {
     console.error(message, error)
-    log.error(message, error)
   }
 }
 
