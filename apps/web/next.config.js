88c5613244b83be1d590923c31b5603114794293
Sasi <me@sasi.codes>
2022-12-07 22:02:20 +0530

    chore: add datadog

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -43,10 +43,8 @@ const moduleExports = withTM({
         source: '/(.*)',
         headers: [
           { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'DENY' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'Referrer-Policy', value: 'strict-origin' },
-          { key: 'Permissions-Policy', value: 'interest-cohort=()' }
+          { key: 'Referrer-Policy', value: 'strict-origin' }
         ]
       }
     ]
