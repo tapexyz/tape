3fa65bbbb8304aa23e9d98d55040b7aa8d9f8013
sasi.eth <me@sasi.codes>
2022-12-07 22:13:49 +0530

    chore: remove axiom (#433)
    * chore: remove axiom
    
    * chore: error boundary
    
    Co-Authored-By: bigint <69431456+bigint@users.noreply.github.com>
    
    * chore: add datadog
    
    * chore: update datadog env
    
    * chore: update env name
    
    * chore: remove env
    
    Co-authored-by: bigint <69431456+bigint@users.noreply.github.com>

---
+++ apps/web/src/components/Common/ErrorBoundary.tsx
@@ -0,0 +1,26 @@
+import type { ReactNode } from 'react'
+import React, { Component } from 'react'
+import Custom500 from 'src/pages/500'
+import logger from 'utils/logger'
+
+interface Props {
+  children?: ReactNode
+}
+interface State {
+  hasError: boolean
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public componentDidCatch(error: Error) {
+    logger.error('[ERROR BOUNDARY]', error)
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <Custom500 />
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
