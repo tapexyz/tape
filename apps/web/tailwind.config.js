14a9c566bc645246ed3527cedeeaa0d01d5d0e8d
Sasi <me@sasi.codes>
2022-12-10 20:11:18 +0530

    fix: tailwind compilation

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import ('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', '../../packages/utils/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
