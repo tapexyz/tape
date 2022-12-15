5d1cd6ce5d1eaf5d53a7454845a66e3496a3539c
Sasi <me@sasi.codes>
2022-12-15 22:17:22 +0530

    chore: update validation

--- apps/web/src/components/Echo/index.tsx
+++ apps/web/src/components/Echo/index.tsx
@@ -1,9 +1,18 @@
+import useAppStore from '@lib/store'
 import React from 'react'
+import { FEATURE_FLAGS } from 'utils/data/feature-flags'
+import getIsFeatureEnabled from 'utils/functions/getIsFeatureEnabled'
 
 import Curated from './Curated'
 import Wrapper from './Wrapper'
 
 const Echo = () => {
+  const selectedChannel = useAppStore((state) => state.selectedChannel)
+
+  if (!getIsFeatureEnabled(FEATURE_FLAGS.LENSTUBE_ECHOS, selectedChannel?.id)) {
+    return null
+  }
+
   return (
     <Wrapper>
       <Curated />
