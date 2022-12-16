a2554f9f14f97bf51f2d15e4e2ec7db71838725b
Sasi <me@sasi.codes>
2022-12-16 13:07:07 +0530

    fix: container alignment

--- apps/web/src/components/Echos/Wrapper.tsx
+++ apps/web/src/components/Echos/Wrapper.tsx
@@ -11,7 +11,7 @@ const Wrapper: FC<Props> = ({ children }) => {
   const selectedTrack = useEchoStore((state) => state.selectedTrack)
 
   return (
-    <div className="max-w-[100rem] relative">
+    <div className="max-w-[100rem] relative mx-auto">
       {children}
       {selectedTrack && (
         <div className="sticky bottom-0 z-10 w-full p-2 bg-white border-t border-x rounded-t-xl dark:border-gray-700 dark:bg-black backdrop-blur-lg">
