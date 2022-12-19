fe0be46b86e4346fc60549b85082b83cf308f4c3
Sasi <me@sasi.codes>
2022-12-19 21:15:19 +0530

    feat: remove ssr in embed player

--- apps/embed/src/pages/[pubId].tsx
+++ apps/embed/src/pages/[pubId].tsx
@@ -1,38 +1,3 @@
 import Video from '@components/Video'
-import { PublicationDetailsDocument } from 'lens'
-import type { GetServerSideProps } from 'next'
-import type { LenstubePublication } from 'utils'
-import { LENSTUBE_APP_ID, LENSTUBE_BYTES_APP_ID } from 'utils'
-import getApolloClient from 'utils/functions/getApolloClient'
 
 export default Video
-
-interface Props {
-  video: LenstubePublication
-}
-
-const apolloClient = getApolloClient()
-
-export const getServerSideProps: GetServerSideProps<Props> = async (
-  context
-) => {
-  const pubId = context.query.pubId as string
-  const splitted = pubId.split('-')
-  if (splitted.length !== 2) {
-    return { notFound: true }
-  }
-  context.res.setHeader('Cache-Control', 'public, s-maxage=86400')
-  const { data, error } = await apolloClient.query({
-    query: PublicationDetailsDocument,
-    variables: {
-      request: { publicationId: pubId },
-      sources: [LENSTUBE_APP_ID, LENSTUBE_BYTES_APP_ID]
-    }
-  })
-  if (!data.publication || error) {
-    return { notFound: true }
-  }
-  return {
-    props: { video: data.publication }
-  }
-}
--- apps/embed/src/pages/_app.tsx
+++ apps/embed/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/index.css'
 
+import { ApolloProvider } from '@apollo/client'
 import { LivepeerConfig } from '@livepeer/react'
 import mixpanel from 'mixpanel-browser'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import { MIXPANEL_API_HOST, MIXPANEL_TOKEN } from 'utils'
+import getApolloClient from 'utils/functions/getApolloClient'
 import { getLivepeerClient, videoPlayerTheme } from 'utils/functions/livepeer'
+
+const apolloClient = getApolloClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   if (MIXPANEL_TOKEN) {
@@ -15,7 +19,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   }
   return (
     <LivepeerConfig client={getLivepeerClient()} theme={videoPlayerTheme}>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </LivepeerConfig>
   )
 }
