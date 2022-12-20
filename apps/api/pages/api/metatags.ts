e386b3bc74cad08dea04dfc1b8afc3967522bd93
Sasi <me@sasi.codes>
2022-12-20 12:44:25 +0530

    chore: update metatags

--- apps/api/pages/api/metatags.ts
+++ apps/api/pages/api/metatags.ts
@@ -8,24 +8,24 @@ import { getRandomProfilePicture } from 'utils/functions/getRandomProfilePicture
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') return res.status(405).json({ success: false })
 
-  const path = req.query.path as string
+  const uri = req.query.path as string
 
-  if (!path) return res.status(400).json({ success: false })
+  if (!uri) return res.status(400).json({ success: false })
 
-  const isChannel = path.includes('/channel/')
-  const isVideo = path.includes('/watch/')
-  const isByte = path.includes('/bytes/')
+  const isChannel = uri.includes('/channel/')
+  const isVideo = uri.includes('/watch/')
+  const isByte = uri.includes('/bytes/')
 
   try {
     if (isChannel) {
-      const handle = path.replace('/channel/', '')
+      const handle = uri.replace('/channel/', '')
       return getProfileMeta(res, handle)
     }
 
     if (isVideo || isByte) {
       const pubId = isByte
-        ? path.replace('/bytes/', '')
-        : path.replace('/watch/', '')
+        ? uri.replace('/bytes/', '')
+        : uri.replace('/watch/', '')
       return getPublicationMeta(res, pubId)
     }
 
