26c783ec92cc92c4ac73dc4cc9d86f1956c08272
Sasi <me@sasi.codes>
2022-12-28 15:44:39 +0530

    chore: remove clear

--- packages/utils/functions/clearLocalStorage.ts
+++ packages/utils/functions/clearLocalStorage.ts
@@ -2,6 +2,5 @@ const clearLocalStorage = () => {
   localStorage.removeItem('accessToken')
   localStorage.removeItem('refreshToken')
   localStorage.removeItem('lenstube.store')
-  localStorage.clear()
 }
 export default clearLocalStorage
