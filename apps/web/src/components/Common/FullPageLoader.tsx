dffbb554073d342f0f9d0234cb3ed2c95f80c60a
Sasi <me@sasi.codes>
2023-01-04 00:26:45 +0530

    chore: update loader logo

--- apps/web/src/components/Common/FullPageLoader.tsx
+++ apps/web/src/components/Common/FullPageLoader.tsx
@@ -10,7 +10,7 @@ const FullPageLoader = () => {
       <MetaTags />
       <div className="animate-bounce">
         <img
-          src={imageCdn(`${STATIC_ASSETS}/images/brand/christmas.png`)}
+          src={imageCdn(`${STATIC_ASSETS}/images/brand/circle-blue-72x72.png`)}
           draggable={false}
           className="w-20 h-20 ml-6 mb-6"
           alt="lenstube"
