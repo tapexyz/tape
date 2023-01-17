b50af19a5969e94d45f76419cd44f6503d57f6b2
Sasi <me@sasi.codes>
2023-01-17 18:40:17 +0530

    chore: remove origin

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -34,7 +34,8 @@ export const LENSTUBE_MAINNET_API_URL = 'https://api.lenstube.xyz'
 export const LENSTUBE_TESTNET_API_URL = 'https://api-testnet.lenstube.xyz'
 export const LENSTUBE_DEV_API_URL = 'http://localhost:5555'
 export const LENSTUBE_API_URL = IS_MAINNET
-  ? 'https://api.lenstube.xyz'
+  ? // ? 'https://api.lenstube.xyz'
+    'https://api-git-video-views-lenstube.vercel.app'
   : 'https://api-testnet.lenstube.xyz'
 
 export const API_URL = IS_MAINNET ? MAINNET_API_URL : TESTNET_API_URL
