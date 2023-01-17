6248f95762ce5c7536a2edffd55b38948b54a3ee
Sasi <me@sasi.codes>
2023-01-17 20:48:43 +0530

    chore: update cache control

--- apps/api/pages/api/video/views.ts
+++ apps/api/pages/api/video/views.ts
@@ -39,7 +39,7 @@ const views = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
       if (!views || !views[0]) {
         return res.status(200).json({ success: false })
       }
-      return res.setHeader('Cache-Control', 's-maxage=60').status(200).json({
+      return res.setHeader('Cache-Control', 's-maxage=100').status(200).json({
         success: true,
         views: views[0].startViews
       })
