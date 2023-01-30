19c828c254fb510fdbefaff591b451abfda58c74
Sasi <me@sasi.codes>
2023-01-30 16:52:20 +0530

    chore: redesign user menu

---
+++ apps/api/pages/api/health.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { BETTER_UPTIME_KEY } from 'utils'
+
+type Monitor = {
+  id: string
+  attributes: {
+    status: string
+  }
+}
+
+const health = async (_req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { data: response } = await axios.get(
+      'https://betteruptime.com//api/v2/monitors',
+      {
+        headers: {
+          Authorization: `Bearer ${BETTER_UPTIME_KEY}`
+        }
+      }
+    )
+    const monitors: Monitor[] = response.data
+    const incidents = monitors.filter(
+      (m: Monitor) => m.attributes.status !== 'up'
+    )
+    return res.status(200).json({ ok: !incidents.length })
+  } catch (error) {
+    return res.status(500).json({ ok: false })
+  }
+}
+
+export default health
--- apps/web/src/components/Common/UserMenu.tsx
+++ apps/web/src/components/Common/UserMenu.tsx
@@ -2,14 +2,24 @@ import DropMenu, { NextLink } from '@components/UIElements/DropMenu'
 import { Menu } from '@headlessui/react'
 import useAppStore from '@lib/store'
 import usePersistStore, { signOut } from '@lib/store/persist'
+import axios from 'axios'
+import clsx from 'clsx'
 import type { Profile } from 'lens'
 import { useAllProfilesLazyQuery } from 'lens'
 import Link from 'next/link'
 import { useTheme } from 'next-themes'
 import React, { useState } from 'react'
 import { toast } from 'react-hot-toast'
+import { useQuery } from 'react-query'
 import type { CustomErrorWithData } from 'utils'
-import { ADMIN_IDS, Analytics, IS_MAINNET, TRACK } from 'utils'
+import {
+  ADMIN_IDS,
+  Analytics,
+  IS_MAINNET,
+  LENSTUBE_API_URL,
+  LENSTUBE_STATUS_PAGE,
+  TRACK
+} from 'utils'
 import getProfilePicture from 'utils/functions/getProfilePicture'
 import { useAccount, useDisconnect } from 'wagmi'
 
@@ -39,6 +49,12 @@ const UserMenu = () => {
   )
   const setSelectedChannelId = usePersistStore(
     (state) => state.setSelectedChannelId
+  )
+
+  const { data: statusData } = useQuery(
+    'statusData',
+    () => axios.get(`${LENSTUBE_API_URL}/api/health`).then(({ data }) => data),
+    { refetchInterval: 60000 }
   )
 
   const [getChannels] = useAllProfilesLazyQuery()
@@ -87,161 +103,182 @@ const UserMenu = () => {
         </button>
       }
     >
-      <div className="px-1 mt-1.5 w-48 divide-y shadow max-h-96 divide-gray-200 dark:divide-gray-800 overflow-hidden border border-gray-100 rounded-xl dark:border-gray-800 bg-secondary dark:bg-theme">
-        {showAccountSwitcher ? (
-          <>
-            <button
-              type="button"
-              className="flex opacity-70 pl-2 outline-none items-center space-x-2"
-              onClick={() => setShowAccountSwitcher(false)}
-            >
-              <ChevronLeftOutline className="w-3 h-3" />
-              <span className="py-2 text-sm">Channels</span>
-            </button>
-            <div className="py-1 text-sm">
-              {channels?.map((channel) => (
-                <button
-                  type="button"
-                  className="flex w-full justify-between items-center px-2 py-1.5 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-                  key={channel.id}
-                  onClick={() => onSelectChannel(channel)}
-                >
-                  <span className="inline-flex items-center space-x-1.5">
-                    <img
-                      className="w-6 h-6 rounded-lg"
-                      src={getProfilePicture(channel)}
-                      alt={channel.handle}
-                      draggable={false}
-                    />
-                    <span className="truncate whitespace-nowrap">
-                      {channel.handle}
-                    </span>
-                  </span>
-                  {selectedChannel?.id === channel.id && (
-                    <CheckOutline className="w-3 h-3" />
-                  )}
-                </button>
-              ))}
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="flex flex-col space-y-1 text-sm transition duration-150 ease-in-out rounded-lg">
-              <div className="inline-flex items-center p-2 py-3 space-x-2 rounded-lg">
-                <img
-                  className="object-cover rounded-full w-9 h-9"
-                  src={getProfilePicture(selectedChannel, 'avatar')}
-                  alt={selectedChannel.handle}
-                  draggable={false}
-                />
-                <div className="grid">
-                  <span className="text-xs opacity-70">Connected as</span>
-                  <Link href={`/channel/${selectedChannel?.handle}`}>
-                    <h6
-                      title={selectedChannel?.handle}
-                      className="text-base truncate leading-4"
-                    >
-                      {selectedChannel?.handle}
-                    </h6>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="py-1 text-sm">
-              {isAdmin && (
-                <Menu.Item
-                  as={NextLink}
-                  href="/stats"
-                  className="inline-flex items-center w-full px-2 py-2 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-                >
-                  <GraphOutline className="w-4 h-4" />
-                  <span className="truncate whitespace-nowrap">App Info</span>
-                </Menu.Item>
-              )}
-              {selectedChannel && (
-                <>
-                  <Menu.Item
-                    as={NextLink}
-                    href={`/channel/${selectedChannel?.handle}`}
-                    className="inline-flex items-center w-full p-2 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-                  >
-                    <ChannelOutline className="w-4 h-4" />
-                    <span className="truncate whitespace-nowrap">
-                      Your Channel
-                    </span>
-                  </Menu.Item>
+      <div className="mt-2 w-56 overflow-hidden rounded-xl dark:bg-black bg-gray-100">
+        <div className="m-1.5 dark:bg-theme bg-white rounded-xl overflow-hidden">
+          {showAccountSwitcher ? (
+            <>
+              <button
+                type="button"
+                className="flex opacity-70 pl-2 outline-none items-center space-x-2"
+                onClick={() => setShowAccountSwitcher(false)}
+              >
+                <ChevronLeftOutline className="w-3 h-3" />
+                <span className="py-2 text-sm">Channels</span>
+              </button>
+              <div className="py-1 text-sm">
+                {channels?.map((channel) => (
                   <button
                     type="button"
-                    className="inline-flex items-center w-full p-2 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-                    onClick={() => {
-                      onSelectSwitchChannel()
-                      Analytics.track(TRACK.CLICK_SWITCH_CHANNEL)
-                    }}
+                    className="flex w-full justify-between items-center px-2 py-1.5 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
+                    key={channel.id}
+                    onClick={() => onSelectChannel(channel)}
                   >
-                    <SwitchChannelOutline className="w-4 h-4" />
+                    <span className="inline-flex items-center space-x-1.5">
+                      <img
+                        className="w-6 h-6 rounded-lg"
+                        src={getProfilePicture(channel)}
+                        alt={channel.handle}
+                        draggable={false}
+                      />
+                      <span className="truncate whitespace-nowrap">
+                        {channel.handle}
+                      </span>
+                    </span>
+                    {selectedChannel?.id === channel.id && (
+                      <CheckOutline className="w-3 h-3" />
+                    )}
+                  </button>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-col space-y-1 text-sm transition duration-150 ease-in-out rounded-lg">
+                <div className="inline-flex items-center p-3 space-x-2 rounded-lg">
+                  <Link href={`/channel/${selectedChannel?.handle}`}>
+                    <img
+                      className="object-cover rounded-full w-9 h-9"
+                      src={getProfilePicture(selectedChannel, 'avatar')}
+                      alt={selectedChannel.handle}
+                      draggable={false}
+                    />
+                  </Link>
+                  <div className="grid">
+                    <span className="text-xs opacity-70">Connected as</span>
+                    <Link href={`/channel/${selectedChannel?.handle}`}>
+                      <h6
+                        title={selectedChannel?.handle}
+                        className="text-base truncate leading-4"
+                      >
+                        {selectedChannel?.handle}
+                      </h6>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="text-sm">
+                {isAdmin && (
+                  <Menu.Item
+                    as={NextLink}
+                    href="/stats"
+                    className="inline-flex items-center w-full px-3 py-2 space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800"
+                  >
+                    <GraphOutline className="w-4 h-4" />
+                    <span className="truncate whitespace-nowrap">App Info</span>
+                  </Menu.Item>
+                )}
+                {selectedChannel && (
+                  <>
+                    <Menu.Item
+                      as={NextLink}
+                      href={`/channel/${selectedChannel?.handle}`}
+                      className="inline-flex items-center w-full px-3 py-2 space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800"
+                    >
+                      <ChannelOutline className="w-4 h-4" />
+                      <span className="truncate whitespace-nowrap">
+                        Your Channel
+                      </span>
+                    </Menu.Item>
+                    <button
+                      type="button"
+                      className="inline-flex items-center w-full px-3 py-2 space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800"
+                      onClick={() => {
+                        onSelectSwitchChannel()
+                        Analytics.track(TRACK.CLICK_SWITCH_CHANNEL)
+                      }}
+                    >
+                      <SwitchChannelOutline className="w-4 h-4" />
+                      <span className="truncate whitespace-nowrap">
+                        Switch channel
+                      </span>
+                    </button>
+                  </>
+                )}
+                {!IS_MAINNET && (
+                  <button
+                    type="button"
+                    className="flex items-center w-full px-3 py-2 space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800"
+                    onClick={() => setShowCreateChannel(true)}
+                  >
+                    <PlusOutline className="w-4 h-4" />
                     <span className="truncate whitespace-nowrap">
-                      Switch channel
+                      Create Channel
                     </span>
                   </button>
-                </>
-              )}
-              {!IS_MAINNET && (
+                )}
+                <Link
+                  href="/settings"
+                  className="flex items-center w-full px-3 py-2 space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800"
+                >
+                  <CogOutline className="w-4 h-4" />
+                  <span className="truncate whitespace-nowrap">
+                    Channel Settings
+                  </span>
+                </Link>
                 <button
                   type="button"
-                  className="flex items-center w-full p-2 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-                  onClick={() => setShowCreateChannel(true)}
+                  className="flex items-center w-full px-3 py-2 space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800"
+                  onClick={() => {
+                    setTheme(theme === 'dark' ? 'light' : 'dark')
+                    Analytics.track(
+                      theme === 'dark'
+                        ? TRACK.SYSTEM.THEME.LIGHT
+                        : TRACK.SYSTEM.THEME.DARK
+                    )
+                  }}
                 >
-                  <PlusOutline className="w-4 h-4" />
+                  {theme === 'dark' ? (
+                    <SunOutline className="w-4 h-4" />
+                  ) : (
+                    <MoonOutline className="w-4 h-4" />
+                  )}
                   <span className="truncate whitespace-nowrap">
-                    Create Channel
+                    {theme === 'light' ? 'Switch to Dark' : 'Switch to Light'}
                   </span>
                 </button>
-              )}
-              <Link
-                href="/settings"
-                className="flex items-center w-full p-2 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-              >
-                <CogOutline className="w-4 h-4" />
-                <span className="truncate whitespace-nowrap">
-                  Channel Settings
-                </span>
-              </Link>
-              <button
-                type="button"
-                className="flex items-center w-full p-2 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-                onClick={() => {
-                  setTheme(theme === 'dark' ? 'light' : 'dark')
-                  Analytics.track(
-                    theme === 'dark'
-                      ? TRACK.SYSTEM.THEME.LIGHT
-                      : TRACK.SYSTEM.THEME.DARK
-                  )
-                }}
-              >
-                {theme === 'dark' ? (
-                  <SunOutline className="w-4 h-4" />
-                ) : (
-                  <MoonOutline className="w-4 h-4" />
-                )}
-                <span className="truncate whitespace-nowrap">
-                  {theme === 'light' ? 'Switch to Dark' : 'Switch to Light'}
-                </span>
-              </button>
-              <button
-                type="button"
-                className="flex items-center w-full px-2.5 py-2 space-x-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
-                onClick={() => {
-                  disconnect?.()
-                  signOut()
-                  Analytics.track(TRACK.AUTH.CLICK_SIGN_OUT)
-                }}
-              >
-                <HandWaveOutline className="w-4 h-4" />
-                <span className="truncate whitespace-nowrap">Sign out</span>
-              </button>
-            </div>
-          </>
-        )}
+                <button
+                  type="button"
+                  className="flex items-center w-full px-3 py-2 space-x-2 hover:bg-gray-100 dark:hover:bg-gray-800"
+                  onClick={() => {
+                    disconnect?.()
+                    signOut()
+                    Analytics.track(TRACK.AUTH.CLICK_SIGN_OUT)
+                  }}
+                >
+                  <HandWaveOutline className="w-4 h-4" />
+                  <span className="truncate whitespace-nowrap">Sign out</span>
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+        <Link
+          className="px-5 m-0.5 pb-3 pt-2 flex items-center space-x-2"
+          href={LENSTUBE_STATUS_PAGE}
+          target="_blank"
+        >
+          <span
+            className={clsx(
+              'rounded-full h-2 w-2',
+              statusData.ok ? 'bg-green-500' : 'bg-red-500'
+            )}
+          />
+          <span className="text-xs">
+            {statusData.ok
+              ? 'All services are online'
+              : 'Some services are offline'}
+          </span>
+        </Link>
       </div>
     </DropMenu>
   )
