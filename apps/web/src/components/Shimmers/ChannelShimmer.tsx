757337d8e6a1314f263f30b9612f8f4405c7e766
Sasi <me@sasi.codes>
2023-01-31 21:35:56 +0530

    chore: update shimmer

--- apps/web/src/components/Shimmers/ChannelShimmer.tsx
+++ apps/web/src/components/Shimmers/ChannelShimmer.tsx
@@ -5,15 +5,16 @@ import TimelineShimmer from './TimelineShimmer'
 
 const ChannelShimmer = () => {
   return (
-    <div className="w-full rounded-md">
-      <div className="flex animate-pulse flex-col md:space-x-4">
-        <div className="h-44 bg-gray-300 dark:bg-gray-700 md:h-72" />
-        <div className="flex items-center space-x-4 p-2">
-          <div className="h-20 w-20 flex-none rounded-xl border-4 bg-gray-300 dark:border-gray-900 dark:bg-gray-700 md:-mt-10 md:h-32 md:w-32" />
+    <>
+      <div className="flex animate-pulse flex-col">
+        <div className="ultrawide:h-[35vh] h-44 bg-gray-300 dark:bg-gray-700 md:h-[20vw]" />
+        <div className="mx-auto flex w-full max-w-[85rem] items-center space-x-4 p-2 md:py-5">
+          <div className="h-20 w-20 flex-none rounded-full bg-gray-300 dark:bg-gray-700 md:h-32 md:w-32" />
           <div className="flex w-full items-end">
             <div className="grid flex-1 grid-cols-2 gap-2">
-              <div className="col-span-2 h-4 rounded bg-gray-300 dark:bg-gray-700 md:w-1/3" />
-              <div className="col-span-1 h-3 rounded bg-gray-300 dark:bg-gray-700 md:w-1/3" />
+              <div className="col-span-2 h-5 rounded bg-gray-300 dark:bg-gray-700 md:w-1/3" />
+              <div className="col-span-2 h-4 rounded bg-gray-300 dark:bg-gray-700 md:w-1/4" />
+              <div className="col-span-1 h-3.5 rounded bg-gray-300 dark:bg-gray-700 md:w-1/3" />
             </div>
             <div className="hidden md:block">
               <ButtonShimmer />
@@ -21,10 +22,20 @@ const ChannelShimmer = () => {
           </div>
         </div>
       </div>
-      <div className="mt-4 md:mt-6">
+      <div className="mx-auto max-w-[85rem] p-2">
+        <div className="flex items-center space-x-2 pb-5 pt-2">
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+          <div className="h-8 w-20 rounded-full bg-gray-300 dark:bg-gray-700" />
+        </div>
         <TimelineShimmer />
       </div>
-    </div>
+    </>
   )
 }
 
