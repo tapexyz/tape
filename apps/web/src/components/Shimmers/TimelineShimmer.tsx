0f993b2fe0be58761c30c85c7f02b983f48cf080
Sasi <me@sasi.codes>
2023-02-01 11:46:18 +0530

    chore: update shimmers

--- apps/web/src/components/Shimmers/TimelineShimmer.tsx
+++ apps/web/src/components/Shimmers/TimelineShimmer.tsx
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react'
 import VideoCardShimmer from './VideoCardShimmer'
 
 const TimelineShimmer = () => {
-  const cards = useMemo(() => Array(6).fill(1), [])
+  const cards = useMemo(() => Array(16).fill(1), [])
   return (
     <div className="ultrawide:grid-cols-6 laptop:grid-cols-4 grid-col-1 grid gap-x-4 gap-y-2 md:grid-cols-2 md:gap-y-8 2xl:grid-cols-5">
       {cards.map((i, idx) => (
