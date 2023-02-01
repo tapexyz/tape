c5d3e4a4e04122e5179dd66a54056ece520975d2
Sasi <me@sasi.codes>
2023-02-02 00:12:22 +0530

    chore: remove consoles

--- packages/utils/functions/getChannelCoverPicture.ts
+++ packages/utils/functions/getChannelCoverPicture.ts
@@ -3,10 +3,6 @@ import type { Profile } from 'lens'
 import { STATIC_ASSETS } from '../constants'
 
 const getChannelCoverPicture = (channel: Profile): string => {
-  console.log(
-    '🚀 ~ file: getChannelCoverPicture.ts:6 ~ getChannelCoverPicture ~ channel',
-    channel
-  )
   return channel.coverPicture && channel.coverPicture.__typename === 'MediaSet'
     ? channel?.coverPicture?.original?.url
     : `${STATIC_ASSETS}/images/coverGradient.jpeg`
