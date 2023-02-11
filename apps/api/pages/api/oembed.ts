4b8435bda3f6f0fdbc0a498d46225713c8619927
Sasi <me@sasi.codes>
2023-02-12 00:28:15 +0530

    chore: add cache

--- apps/api/pages/api/oembed.ts
+++ apps/api/pages/api/oembed.ts
@@ -13,12 +13,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (format === 'json') {
       return res
         .setHeader('Content-Type', 'application/json')
+        .setHeader('Cache-Control', 's-maxage=86400')
         .json(await getPublicationOembed(videoId, format))
     }
 
     if (format === 'xml') {
       return res
         .setHeader('Content-Type', 'application/xml')
+        .setHeader('Cache-Control', 's-maxage=86400')
         .send(await getPublicationOembed(videoId, format))
     }
 
