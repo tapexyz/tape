255c290d4ad2a07d1413336dca02758e72047cdc
Sasi <me@sasi.codes>
2023-02-11 22:12:58 +0530

    chore: update logs (#605)
    * chore: update logs
    
    * chore: add worker script

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -100,6 +100,7 @@ export const LENSTUBE_GITHUB_HANDLE = 'lenstube-xyz'
 export const LENSTUBE_STATUS_PAGE = 'https://status.lenstube.xyz'
 export const TALLY_VERIFICATION_FORM_URL = 'https://tally.so/r/mY5e80'
 export const LENSTUBE_ROADMAP_URL = 'https://roadmap.lenstube.xyz'
+export const LENSTUBE_TAIL_URL = 'https://tail.lenstube.xyz'
 
 // admin
 export const ADMIN_IDS = IS_MAINNET ? ['0x2d'] : ['0x2f']
@@ -130,7 +131,6 @@ export const RARIBLE_MARKETPLACE_URL = IS_MAINNET
 export const MIXPANEL_API_HOST = '/collect'
 export const MIXPANEL_TOKEN = '928986a6551f5a12132e63c8bdd4451e'
 export const MUX_DATA_KEY = '2h11sq1qeahiaejrjegjti847'
-export const DATADOG_API_KEY = process.env.NEXT_PUBLIC_DATADOG_API_KEY
 export const BETTER_UPTIME_KEY = process.env.BETTER_UPTIME_KEY
 
 // vercel
--- packages/utils/logger.ts
+++ packages/utils/logger.ts
@@ -1,31 +1,18 @@
 import axios from 'axios'
-import { v4 as uuid } from 'uuid'
 
-import { DATADOG_API_KEY, IS_MAINNET } from './constants'
+import { IS_MAINNET, LENSTUBE_TAIL_URL } from './constants'
 
-const enabled = DATADOG_API_KEY && IS_MAINNET
-const isBrowser = typeof window !== 'undefined'
-
-const sendError = (error: string) => {
-  if (isBrowser && enabled) {
-    const reqId = uuid()
-    axios('https://http-intake.logs.datadoghq.com/api/v2/logs', {
-      method: 'POST',
-      params: {
-        'dd-api-key': DATADOG_API_KEY,
-        'dd-request-id': reqId
-      },
-      data: {
-        ddsource: 'browser',
-        status: 'error',
-        error,
-        url: location.href,
-        sha: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
-      }
-    })
-      .then(() => reqId)
-      .catch(() => {
-        console.error('Error while sending error to Datadog')
+const tailLog = (level: 'error' | 'log', message: string) => {
+  if (IS_MAINNET) {
+    axios
+      .post(LENSTUBE_TAIL_URL, {
+        source: 'web',
+        level,
+        message,
+        url: location.href
+      })
+      .catch((error) => {
+        console.error(error)
       })
   }
 }
@@ -38,7 +25,7 @@ const logger = {
     console.warn(...args)
   },
   error: (message: string, error: any) => {
-    sendError(`${message} - ${JSON.stringify(error)}`)
+    tailLog('error', `${message} ${JSON.stringify(error)}`)
     console.error(message, error)
   }
 }
