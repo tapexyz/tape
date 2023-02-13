dea4d13c25012649a94068ac89741718bb733968
Dimitris Apostolou <dimitris.apostolou@icloud.com>
2023-02-13 19:47:40 +0200

    fix: typos

--- packages/lens/generated.ts
+++ packages/lens/generated.ts
@@ -2783,7 +2783,7 @@ export enum PublicationMediaSource {
   Lens = 'LENS'
 }
 
-/** Publication metadata content waring filters */
+/** Publication metadata content warning filters */
 export type PublicationMetadataContentWarningFilter = {
   /** By default all content warnings will be hidden you can include them in your query by adding them to this array. */
   includeOneOf?: InputMaybe<Array<PublicationContentWarning>>
@@ -3332,7 +3332,7 @@ export type ReferenceModule =
   | UnknownReferenceModuleSettings
 
 export type ReferenceModuleParams = {
-  /** The degrees of seperation reference module */
+  /** The degrees of separation reference module */
   degreesOfSeparationReferenceModule?: InputMaybe<DegreesOfSeparationReferenceModuleParams>
   /** The follower only reference module */
   followerOnlyReferenceModule?: InputMaybe<Scalars['Boolean']>
