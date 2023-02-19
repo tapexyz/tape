40614acd3394a183af979c276c472a5df0ada3f6
Sasi <me@sasi.codes>
2023-02-19 13:33:18 +0530

    chore: remove tinybird (#633)

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 import React from 'react'
-import { IS_MAINNET, STATIC_ASSETS } from 'utils'
+import { STATIC_ASSETS } from 'utils'
 
 class LenstubeDocument extends Document {
   render() {
@@ -33,17 +32,6 @@ class LenstubeDocument extends Document {
           />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#000000" />
-
-          {IS_MAINNET && (
-            <Script
-              strategy="lazyOnload"
-              id="tinybird"
-              defer
-              src="https://unpkg.com/@tinybirdco/flock.js"
-              data-host="https://api.tinybird.co"
-              data-token="p.eyJ1IjogImI1YzEwMWY0LTY0MmMtNDJhNy1hZmMxLWIwMjNkZjU2ZWY5YiIsICJpZCI6ICIzY2RmMjVmNi1lMmQ4LTQ3MGItYmFlMy02MDBlMDU4MDQyN2EifQ.67XL26aJbQyvWHEFpyzg38DA3OHRpdIVsQ4FdyD2G5A"
-            />
-          )}
         </Head>
         <body>
           <Main />
