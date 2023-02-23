305b80a9460a9a08e8b1820669b5dfd4d4d60f74
Sasi <me@sasi.codes>
2023-02-23 12:27:48 +0530

    chore: update verified members

--- packages/utils/data/verified/index.ts
+++ packages/utils/data/verified/index.ts
@@ -2,6 +2,6 @@ import { IS_MAINNET } from '../../constants'
 import { AAVE_MEMBERS } from './aave-members'
 import { CREATORS } from './creators'
 
-export const CORE_MEMBERS = IS_MAINNET ? ['0x2d'] : ['0x2f'] // sasicodes
+export const CORE_MEMBERS = IS_MAINNET ? ['0x2d', '0xc001'] : ['0x2f'] // sasicodes
 
 export const VERIFIED_CHANNELS = [...CREATORS, ...AAVE_MEMBERS, ...CORE_MEMBERS]
