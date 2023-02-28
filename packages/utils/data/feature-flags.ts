a32ff559a2429cf4cb2096e4555097b7465a7aad
Sasi <me@sasi.codes>
2023-02-28 11:43:04 +0530

    chore: remove multicollect feature flag (#660)

--- packages/utils/data/feature-flags.ts
+++ packages/utils/data/feature-flags.ts
@@ -1,13 +1,10 @@
-import { CORE_MEMBERS } from './verified'
-import { AAVE_MEMBERS } from './verified/aave-members'
-
 export enum FEATURE_FLAGS {
   MULTI_COLLECT_MODULE = 'MultiCollectModule'
 }
 
-export const featureFlags = [
-  {
-    flag: FEATURE_FLAGS.MULTI_COLLECT_MODULE,
-    enabledFor: [...AAVE_MEMBERS, ...CORE_MEMBERS]
-  }
-]
+type FeatureFlag = {
+  flag: string
+  enabledFor: string[]
+}
+
+export const featureFlags: FeatureFlag[] = []
