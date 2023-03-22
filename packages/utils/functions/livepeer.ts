833f708f2919ad76a9f07d4fb6b4753f399cc9d8
Sasi <me@sasi.codes>
2023-03-22 21:00:29 +0530

    chore: cleanup

--- packages/utils/functions/livepeer.ts
+++ packages/utils/functions/livepeer.ts
@@ -18,7 +18,7 @@ export const videoPlayerTheme: ThemeConfig = {
     loading: '#6366f1'
   },
   fonts: {
-    display: 'Matter'
+    display: 'system-ui'
   },
   fontSizes: {
     timeFontSize: '12px'
