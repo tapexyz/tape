b370187790b7affe70f11f7cc1578e57e6ef136b
Sasi <me@sasi.codes>
2023-04-02 19:37:13 +0530

    chore: cleanup

--- apps/web/src/components/Watch/Comments/CommentOptions.tsx
+++ apps/web/src/components/Watch/Comments/CommentOptions.tsx
@@ -4,7 +4,6 @@ import TrashOutline from '@components/Common/Icons/TrashOutline'
 import Confirm from '@components/UIElements/Confirm'
 import DropMenu from '@components/UIElements/DropMenu'
 import useChannelStore from '@lib/store/channel'
-import clsx from 'clsx'
 import type { Publication } from 'lens'
 import { useHidePublicationMutation } from 'lens'
 import type { Dispatch, FC } from 'react'
@@ -15,14 +14,9 @@ import { Analytics, TRACK } from 'utils'
 type Props = {
   setShowReport: Dispatch<boolean>
   comment: Publication
-  showOnHover?: boolean
 }
 
-const CommentOptions: FC<Props> = ({
-  comment,
-  setShowReport,
-  showOnHover = false
-}) => {
+const CommentOptions: FC<Props> = ({ comment, setShowReport }) => {
   const [showConfirm, setShowConfirm] = useState(false)
   const selectedChannel = useChannelStore((state) => state.selectedChannel)
 
@@ -56,13 +50,7 @@ const CommentOptions: FC<Props> = ({
       />
       <DropMenu
         trigger={
-          <div
-            className={clsx(
-              'p-1 group-hover:visible',
-              showOnHover && 'lg:invisible'
-            )}
-            role="button"
-          >
+          <div className="p-1" role="button">
             <ThreeDotsOutline className="h-3.5 w-3.5" />
           </div>
         }
