0659097a519c2e8226bb0c1c918a3ad1c2207e76
Sasi <me@sasi.codes>
2023-04-07 18:39:36 +0530

    chore: cleanup

--- packages/utils/functions/getIsDispatcherEnabled.tsx
+++ packages/utils/functions/getIsDispatcherEnabled.tsx
@@ -1,11 +1,11 @@
 import type { Profile } from 'lens'
-import { RELAYER_ADDRESS } from 'utils'
+import { LENS_RELAYER_ADDRESS } from 'utils'
 
 const getIsDispatcherEnabled = (channel: Profile | null) => {
   return (
     channel?.dispatcher?.canUseRelay &&
     channel.dispatcher?.address?.toLocaleLowerCase() !==
-      RELAYER_ADDRESS.toLocaleLowerCase()
+      LENS_RELAYER_ADDRESS.toLocaleLowerCase()
   )
 }
 
