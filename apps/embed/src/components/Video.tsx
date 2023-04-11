6307a78a773b4ed83c1ee89e03efba6186ea6e8d
Sasi <me@sasi.codes>
2023-04-12 01:12:18 +0530

    fix: circle image

--- apps/embed/src/components/Video.tsx
+++ apps/embed/src/components/Video.tsx
@@ -104,7 +104,7 @@ const Video: FC<Props> = ({ video }) => {
               <img
                 className="h-5 w-5"
                 src={imageCdn(
-                  `${STATIC_ASSETS}/images/brand/white.svg`,
+                  `${STATIC_ASSETS}/images/brand/lenstube.svg`,
                   'avatar'
                 )}
                 alt="play"
