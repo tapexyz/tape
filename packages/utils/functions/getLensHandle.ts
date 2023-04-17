44d00ac7ee862959abe5be1125f2390f20e5ccbf
Sasi <me@sasi.codes>
2023-04-17 10:52:15 +0530

    fix: commented videos

--- packages/utils/functions/getLensHandle.ts
+++ packages/utils/functions/getLensHandle.ts
@@ -1,6 +1,9 @@
 import { IS_MAINNET } from '../constants'
 
 const getLensHandle = (handle: string) => {
+  if (!handle) {
+    return handle
+  }
   const name = handle.replace('.lens', '').replace('.test', '')
   return `${name}.${IS_MAINNET ? 'lens' : 'test'}`
 }
