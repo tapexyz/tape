229fd469995a249569e10dc5c0cc37ad6b59731e
Sasi <me@sasi.codes>
2023-04-22 13:35:27 +0530

    chore: add back events for embed (#775)

--- apps/embed/src/pages/_app.tsx
+++ apps/embed/src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import '../styles/index.css'
 
 import { LivepeerConfig } from '@livepeer/react'
+import mixpanel from 'mixpanel-browser'
 import type { AppProps } from 'next/app'
 import React from 'react'
+import { IS_PRODUCTION, MIXPANEL_API_HOST, MIXPANEL_TOKEN } from 'utils'
 import bloomer from 'utils/font'
 import { getLivepeerClient, videoPlayerTheme } from 'utils/functions/livepeer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // if (IS_PRODUCTION) {
-  //   mixpanel.init(MIXPANEL_TOKEN, {
-  //     api_host: MIXPANEL_API_HOST
-  //   })
-  // }
+  if (IS_PRODUCTION) {
+    mixpanel.init(MIXPANEL_TOKEN, {
+      api_host: MIXPANEL_API_HOST,
+      cross_subdomain_cookie: true
+    })
+  }
   return (
     <LivepeerConfig client={getLivepeerClient()} theme={videoPlayerTheme}>
       <style jsx global>{`
