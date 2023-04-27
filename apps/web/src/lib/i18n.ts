1f866e5cd54d021c95362a74aefff863ffc9f520
Sasi <me@sasi.codes>
2023-04-27 19:28:02 +0530

    chore: add translations

--- apps/web/src/lib/i18n.ts
+++ apps/web/src/lib/i18n.ts
@@ -3,23 +3,20 @@ import dayjs from 'dayjs'
 import { en, es } from 'make-plural/plurals'
 import { DEFAULT_LOCALE, SUPPORTED_LOCALES } from 'utils/constants'
 
-i18n.load({
-  en: { plurals: en },
-  es: { plurals: es }
-})
+export const initLocale = async () => {
+  i18n.load({
+    en: { plurals: en },
+    es: { plurals: es }
+  })
+  let locale = localStorage.getItem('locale')
+  const selectedLocale = locale ? JSON.parse(locale) : DEFAULT_LOCALE
 
-export const setLocale = async (locale: string) => {
-  if (!Object.values(SUPPORTED_LOCALES).includes(locale)) {
+  if (!Object.values(SUPPORTED_LOCALES).includes(selectedLocale)) {
     locale = DEFAULT_LOCALE
   }
   localStorage.setItem('locale', JSON.stringify(locale))
   const { messages } = await import(`src/locales/${locale}/messages`)
-  i18n.load(locale, messages)
-  i18n.activate(locale)
-  dayjs.locale(locale)
-}
-
-export const initLocale = () => {
-  const locale = localStorage.getItem('locale')
-  setLocale(locale ? JSON.parse(locale) : DEFAULT_LOCALE)
+  i18n.load(selectedLocale, messages)
+  i18n.activate(selectedLocale)
+  dayjs.locale(selectedLocale)
 }
