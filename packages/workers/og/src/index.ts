22ed4a909b8894fe086f6cf56ec160cc7414b1b1
Sasi <me@sasi.codes>
2023-04-29 11:36:54 +0530

    chore: add OG worker to packages

---
+++ packages/workers/og/src/index.ts
@@ -0,0 +1,19 @@
+async function handleRequest(request: Request) {
+  const userAgent = request.headers.get('User-Agent') || '';
+  const regex = /bot|telegram|baidu|bing|yandex|iframely|whatsapp|metainspector/i
+
+  if (regex.test(userAgent)) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+    const fetchUrl = `https://api.lenstube.xyz/metatags?path=${path}`;
+    return fetch(fetchUrl);
+  }
+
+  return fetch(request);
+}
+
+export default {
+  async fetch(request: Request) {
+    return await handleRequest(request);
+  }
+};
