36e3d5b7be9c4259415b9aa92d63c5e7d5c7b2a4
Sasi <me@sasi.codes>
2023-06-26 12:56:48 +0530

    chore: bump deps (#897)

--- apps/mobile/src/components/home/FirstSteps.tsx
+++ apps/mobile/src/components/home/FirstSteps.tsx
@@ -64,7 +64,7 @@ const FirstSteps = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>First steps with Pripe</Text>
+      <Text style={styles.title}>First steps with Lenstube</Text>
       <Text style={styles.subheading}>Unleash New Social Horizons</Text>
       <ScrollView
         horizontal={true}
