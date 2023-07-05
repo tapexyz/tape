5123ad047bb6a58aca7fc53d8645858ffa357e60
Sasi <me@sasi.codes>
2023-07-05 20:53:53 +0530

    chore: bump deps (#913)

--- packages/ui/VideoPlayer/SensitiveWarning.tsx
+++ packages/ui/VideoPlayer/SensitiveWarning.tsx
@@ -10,7 +10,7 @@ const SensitiveWarning: FC<Props> = ({ acceptWarning }) => {
     <div className="flex h-56 w-full items-center justify-center space-x-5 rounded-xl bg-black px-10 text-lg text-white md:h-[60vh]">
       <div className="flex flex-col">
         <div className="text-base">
-          The following video has sensitive content and may inappropriate or
+          The following video has sensitive content and may be inappropriate or
           offensive to some audiences. <div>Viewer discretion is advised.</div>
         </div>
         <div>
