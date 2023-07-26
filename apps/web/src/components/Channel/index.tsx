a18e7b96914eddf6eb91dc5bacf3c09ae49e3d64
Sasi <me@sasi.codes>
2023-07-26 12:12:13 +0530

    cleanup

--- apps/web/src/components/Channel/index.tsx
+++ apps/web/src/components/Channel/index.tsx
@@ -44,11 +44,9 @@ const Channel = () => {
   return (
     <>
       <MetaTags title={channel?.handle} />
-
       {!loading && !error && channel ? (
         <>
           <BasicInfo channel={channel} />
-
           <Tabs channel={channel} />
         </>
       ) : null}
