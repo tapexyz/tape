103620b330ec87dd566f2b7d5e4b70150b06b3a2
Sasi <me@sasi.codes>
2023-07-29 10:48:36 +0530

    fix: sharable link

--- packages/helpers/generic/functions/getSharableLink.ts
+++ packages/helpers/generic/functions/getSharableLink.ts
@@ -18,22 +18,22 @@ export const getSharableLink = (link: Link, video: Publication) => {
     return `${LENSTUBE_WEBSITE_URL}/watch/${video.id}`
   } else if (link === 'lenster') {
     return `${LENSTER_WEBSITE_URL}/?url=${getViewUrl(video)}&text=${
-      metadata?.name as string
+      (metadata?.name as string) ?? ''
     } by @${handle}&hashtags=Lenstube&preview=true`
   } else if (link === 'twitter') {
     return encodeURI(
       `https://twitter.com/intent/tweet?url=${getViewUrl(video)}&text=${
-        metadata?.name as string
+        (metadata?.name as string) ?? ''
       } by @${handle}&via=${LENSTUBE_TWITTER_HANDLE}&related=Lenstube&hashtags=Lenstube`
     )
   } else if (link === 'reddit') {
     return `https://www.reddit.com/submit?url=${getViewUrl(video)}&title=${
-      metadata?.name as string
+      (metadata?.name as string) ?? ''
     } by @${handle}`
   } else if (link === 'linkedin') {
     return `https://www.linkedin.com/shareArticle/?url=${getViewUrl(
       video
-    )} by @${handle}&title=${metadata?.name as string}&summary=${
+    )} by @${handle}&title=${(metadata?.name as string) ?? ''}&summary=${
       metadata?.description as string
     }&source=${LENSTUBE_APP_NAME}`
   }
