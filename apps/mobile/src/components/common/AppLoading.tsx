fa9b02afb05b2667e4c82d6dbc92ec277ca59637
Sasi <me@sasi.codes>
2023-08-07 22:19:26 +0530

    feat: splash and icons

--- apps/mobile/src/components/common/AppLoading.tsx
+++ apps/mobile/src/components/common/AppLoading.tsx
@@ -1,30 +1,45 @@
+import { Image as ExpoImage } from 'expo-image'
 import * as SplashScreen from 'expo-splash-screen'
-import { MotiView } from 'moti'
 import type { FC, PropsWithChildren } from 'react'
-import React, { useMemo } from 'react'
-import { View } from 'react-native'
-import Animated, { FadeInUp, FadeOutUp } from 'react-native-reanimated'
+import React, { useState } from 'react'
+import { useWindowDimensions, View } from 'react-native'
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated'
 
 import { theme } from '~/helpers/theme'
 
-import {
-  useAuth,
-  useCachedResources,
-  useEffect,
-  usePlatform
-} from '../../hooks'
+import { useAuth, useCachedResources, useEffect } from '../../hooks'
 
 SplashScreen.preventAutoHideAsync()
 
 const Splash = () => {
-  const text = 'pripe'
-  const FONT_SIZE = 50
-  const delay = 100
+  const { height, width } = useWindowDimensions()
 
-  const reversedText = useMemo(() => {
-    return text.split('').reverse()
-  }, [text])
-  const { isIOS } = usePlatform()
+  const scale = useSharedValue(1)
+  const opacity = useSharedValue(1)
+
+  useEffect(() => {
+    // 3 times scale
+    scale.value = withTiming(3, {
+      duration: 1000,
+      easing: Easing.inOut(Easing.ease)
+    })
+    opacity.value = withTiming(0, {
+      duration: 1000,
+      easing: Easing.inOut(Easing.ease)
+    })
+  }, [scale, opacity])
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+      opacity: opacity.value
+    }
+  })
 
   return (
     <View
@@ -35,64 +50,34 @@ const Splash = () => {
         backgroundColor: theme.colors.background
       }}
     >
-      <MotiView
-        key={text}
-        entering={FadeInUp.delay(reversedText.length * delay).springify()}
-        exiting={FadeOutUp.delay(reversedText.length * delay).springify()}
-        from={{ opacity: 0.6 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          type: 'timing',
-          loop: true,
-          duration: 800,
-          repeatReverse: true,
-          delay: reversedText.length * delay
-        }}
-      >
-        {reversedText.map((char, index) => {
-          return (
-            <Animated.View
-              key={index}
-              entering={FadeInUp.delay(
-                (reversedText.length - index) * delay
-              ).springify()}
-              exiting={FadeOutUp.delay(
-                (reversedText.length - index) * delay
-              ).springify()}
-            >
-              <Animated.Text
-                style={{
-                  fontWeight: 'bold',
-                  color: theme.colors.white,
-                  fontSize: FONT_SIZE,
-                  marginVertical: -FONT_SIZE / 8,
-                  lineHeight: FONT_SIZE,
-                  transform: [{ rotate: '-90deg' }],
-                  textAlign: 'center',
-                  fontFamily: isIOS ? 'Menlo' : 'Roboto'
-                }}
-              >
-                {char}
-              </Animated.Text>
-            </Animated.View>
-          )
-        })}
-      </MotiView>
+      <Animated.View style={animatedStyle}>
+        <ExpoImage
+          source={require('assets/splash.png')}
+          transition={100}
+          contentFit="cover"
+          style={[{ width, height }]}
+        />
+      </Animated.View>
     </View>
   )
 }
 
 export const AppLoading: FC<PropsWithChildren> = ({ children }) => {
+  const [appLoadingIsVisible, setAppLoadingIsVisible] = useState(true)
+
   const isCached = useCachedResources()
   const isAuthValidated = useAuth()
 
   useEffect(() => {
+    SplashScreen.hideAsync()
     if (isCached && isAuthValidated) {
-      SplashScreen.hideAsync()
+      setTimeout(() => {
+        setAppLoadingIsVisible(false)
+      }, 500)
     }
   }, [isCached, isAuthValidated])
 
-  if (!isCached || !isAuthValidated) {
+  if (appLoadingIsVisible) {
     return <Splash />
   }
 
