d9677436c9d590b2567a7a5931ddc9ca338d57a1
Sasi <me@sasi.codes>
2023-08-07 23:45:33 +0530

    update timer

--- apps/mobile/src/components/common/AppLoading.tsx
+++ apps/mobile/src/components/common/AppLoading.tsx
@@ -73,7 +73,7 @@ export const AppLoading: FC<PropsWithChildren> = ({ children }) => {
     if (isCached && isAuthValidated) {
       setTimeout(() => {
         setAppLoadingIsVisible(false)
-      }, 500)
+      }, 100)
     }
   }, [isCached, isAuthValidated])
 
