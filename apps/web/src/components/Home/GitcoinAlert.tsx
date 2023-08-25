309e9d4d802489d8e37d8315e1dbbdfda513a14d
William Lopez-Cordero <williamlopezc@gmail.com>
2023-08-25 11:44:37 -0700

    (nit: remove translate

--- apps/web/src/components/Home/GitcoinAlert.tsx
+++ apps/web/src/components/Home/GitcoinAlert.tsx
@@ -1,6 +1,5 @@
 import HandWaveOutline from '@components/Common/Icons/HandWaveOutline'
 import { Button } from '@components/UIElements/Button'
-import { Trans } from '@lingui/macro'
 import Link from 'next/link'
 import React from 'react'
 import { LENSTUBE_APP_NAME, WINDOW_BACKGROUND_URL } from 'utils/constants'
@@ -16,9 +15,7 @@ const GitcoinAlert: React.FC = () => {
         <div className="relative z-[1] flex flex-col items-start space-y-4 text-left">
           <div className="flex items-center rounded-full bg-gradient-to-br from-orange-200 to-orange-300 px-3 py-1 text-xs font-medium text-black">
             <HandWaveOutline className="h-3.5 w-3.5" />
-            <span className="ml-1">
-              <Trans>GG18 is Live</Trans>
-            </span>
+            <span className="ml-1">GG18 is Live</span>
           </div>
           <div className="flex w-full flex-1 flex-wrap items-center justify-between gap-y-3 dark:text-gray-100">
             <p className="md:text-md text-sm lg:text-lg">
