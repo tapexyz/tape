5f7d2ee58003bb2a9ae3db0517ad3b9d4fe53ba3
Sasi <me@sasi.codes>
2023-08-30 18:56:49 +0530

    fix: lint

--- apps/mobile/src/components/common/markdown/HashtagLink.tsx
+++ apps/mobile/src/components/common/markdown/HashtagLink.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Pressable, StyleSheet, Text } from 'react-native'
 
 import normalizeFont from '~/helpers/normalize-font'
+import { useMobileTheme } from '~/hooks'
 
 export type HashtagLinkProps = {
   hashtag: string
@@ -19,9 +20,11 @@ const styles = (themeConfig: MobileThemeConfig) =>
   })
 
 const HashtagLink = ({ hashtag }: HashtagLinkProps) => {
+  const { themeConfig } = useMobileTheme()
+
   return (
     <Pressable>
-      <Text style={styles.hashtag}>{hashtag}</Text>
+      <Text style={styles(themeConfig).hashtag}>{hashtag}</Text>
     </Pressable>
   )
 }
