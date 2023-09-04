5b67a831942c01e98eea71f91b32ba7ddaafa549
Sasi <me@sasi.codes>
2023-09-04 21:24:09 +0530

    fix: status bar

--- apps/mobile/src/navigation/BytesStack.tsx
+++ apps/mobile/src/navigation/BytesStack.tsx
@@ -1,25 +1,21 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 
-import { StatusBar } from '~/components/common/StatusBar'
 import { BytesScreen } from '~/screens'
 
 const { Navigator, Screen } = createStackNavigator<BytesStackParamList>()
 
 export const BytesStack = (): JSX.Element => {
   return (
-    <>
-      <StatusBar style="light" />
-      <Navigator>
-        <Screen
-          name="Bytes"
-          options={{
-            title: 'Bytes',
-            headerShown: false
-          }}
-          component={BytesScreen}
-        />
-      </Navigator>
-    </>
+    <Navigator>
+      <Screen
+        name="Bytes"
+        options={{
+          title: 'Bytes',
+          headerShown: false
+        }}
+        component={BytesScreen}
+      />
+    </Navigator>
   )
 }
