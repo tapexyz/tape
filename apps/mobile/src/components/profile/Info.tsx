af6c2477452184716f70a6dc4e9b9ea80a83518a
Sasi <me@sasi.codes>
2023-09-07 13:40:08 +0530

    update Info.tsx

--- apps/mobile/src/components/profile/Info.tsx
+++ apps/mobile/src/components/profile/Info.tsx
@@ -36,7 +36,6 @@ import useMobileStore from '~/store'
 
 import UserProfile from '../common/UserProfile'
 import Button from '../ui/Button'
-import Ticker from '../ui/Ticker'
 import OnChainInfo from './OnChainInfo'
 import ShareSheet from './ShareSheet'
 
@@ -72,12 +71,12 @@ const styles = (themeConfig: MobileThemeConfig) =>
     stat: {
       flexDirection: 'column',
       width: windowWidth * 0.3,
-      alignItems: 'center',
-      gap: 4
+      alignItems: 'center'
     },
     tickerText: {
       fontFamily: 'font-bold',
-      color: themeConfig.textColor
+      color: themeConfig.textColor,
+      fontSize: normalizeFont(14)
     },
     handle: {
       fontFamily: 'font-bold',
@@ -201,11 +200,7 @@ const Info: FC<Props> = (props) => {
 
         <View style={style.statsContainer}>
           <View style={style.stat}>
-            <Ticker
-              number={profile.stats.totalFollowers}
-              textStyle={style.tickerText}
-              textSize={18}
-            />
+            <Text style={style.tickerText}>{profile.stats.totalFollowers}</Text>
             <Text style={style.text}>followers</Text>
           </View>
           <View style={{ marginTop: -(height * 0.07) }}>
@@ -218,11 +213,7 @@ const Info: FC<Props> = (props) => {
             />
           </View>
           <View style={style.stat}>
-            <Ticker
-              number={profile.stats.totalCollects}
-              textStyle={style.tickerText}
-              textSize={18}
-            />
+            <Text style={style.tickerText}>{profile.stats.totalCollects}</Text>
             <Text style={style.text}>collects</Text>
           </View>
         </View>
