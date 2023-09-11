f4600883298f54b4d911de556e31aaac28e1f46d
Sasi <me@sasi.codes>
2023-09-11 16:16:19 +0530

    update AppLoading.tsx

--- apps/mobile/src/components/common/AppLoading.tsx
+++ apps/mobile/src/components/common/AppLoading.tsx
@@ -1,24 +1,13 @@
-import { Image as ExpoImage } from 'expo-image'
 import * as SplashScreen from 'expo-splash-screen'
 import type { FC, PropsWithChildren } from 'react'
 import React, { useEffect, useState } from 'react'
-import { StyleSheet } from 'react-native'
+import { ActivityIndicator } from 'react-native'
 
 import { useMobilePersistStore } from '~/store/persist'
 
 import { useAuth, useCachedResources } from '../../hooks'
 
 SplashScreen.preventAutoHideAsync()
-
-const Splash = () => {
-  return (
-    <ExpoImage
-      source={require('assets/splash.png')}
-      contentFit="cover"
-      style={StyleSheet.absoluteFill}
-    />
-  )
-}
 
 const AppLoading: FC<PropsWithChildren> = ({ children }) => {
   const [appLoadingIsVisible, setAppLoadingIsVisible] = useState(true)
@@ -40,7 +29,7 @@ const AppLoading: FC<PropsWithChildren> = ({ children }) => {
   }, [isCached, isAuthValidated, selectedChannelId])
 
   if (appLoadingIsVisible) {
-    return <Splash />
+    return <ActivityIndicator style={{ flex: 1 }} />
   }
 
   // eslint-disable-next-line react/jsx-no-useless-fragment
