0a37c2044c66d274e33760f5a0269f3a3c33106b
Sasi <me@sasi.codes>
2023-09-17 00:04:47 +0530

    update NFTCard.tsx

--- apps/web/src/components/Channel/Tabs/NFTCard.tsx
+++ apps/web/src/components/Channel/Tabs/NFTCard.tsx
@@ -71,6 +71,9 @@ const NFTCard: FC<Props> = ({ nft }) => {
               className="object-contain"
               alt={nft.metaData.name}
               draggable={false}
+              onError={async ({ currentTarget }) => {
+                currentTarget.src = FALLBACK_COVER_URL
+              }}
             />
           </>
         )}
