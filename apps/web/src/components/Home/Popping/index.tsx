9723bee2825ad341ce1bc17046950c7c981243bd
Sasi <me@sasi.codes>
2023-10-05 20:34:56 +0530

    update apps/web/src/components/Home/Popping/index.tsx

--- apps/web/src/components/Home/Popping/index.tsx
+++ apps/web/src/components/Home/Popping/index.tsx
@@ -1,6 +1,4 @@
-import BytesOutline from '@components/Common/Icons/BytesOutline'
-import ChevronLeftOutline from '@components/Common/Icons/ChevronLeftOutline'
-import ChevronRightOutline from '@components/Common/Icons/ChevronRightOutline'
+import ExploreOutline from '@components/Common/Icons/ExploreOutline'
 import { Trans } from '@lingui/macro'
 import React from 'react'
 
@@ -9,21 +7,11 @@ import Unlonely from './Unlonely'
 const WhatsPopping = () => {
   return (
     <div>
-      <div className="mb-4 flex items-center justify-between">
-        <div className="flex items-center space-x-2">
-          <BytesOutline className="h-4 w-4" />
-          <h1 className="text-xl font-semibold">
-            <Trans>What's Popping?</Trans>
-          </h1>
-        </div>
-        <div className="flex justify-end space-x-3">
-          <button className="rounded-full bg-gray-500 bg-opacity-10 p-2 backdrop-blur-xl hover:bg-opacity-25 focus:outline-none">
-            <ChevronLeftOutline className="h-4 w-4" />
-          </button>
-          <button className="rounded-full bg-gray-500 bg-opacity-10 p-2 backdrop-blur-xl hover:bg-opacity-25 focus:outline-none">
-            <ChevronRightOutline className="h-4 w-4" />
-          </button>
-        </div>
+      <div className="mb-4 flex items-center space-x-2">
+        <ExploreOutline className="h-4 w-4" />
+        <h1 className="text-xl font-semibold">
+          <Trans>What's Popping?</Trans>
+        </h1>
       </div>
       <div>
         <Unlonely />
