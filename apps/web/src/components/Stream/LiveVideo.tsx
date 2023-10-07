433ceee28559dc0471fdbd3a1aab6e80aa34654f
sasi.eth <hey@sasi.codes>
2023-10-07 16:02:17 +0530

    update LiveVideo.tsx and apps/web/src/components/Stream/index.tsx

--- apps/web/src/components/Stream/LiveVideo.tsx
+++ apps/web/src/components/Stream/LiveVideo.tsx
@@ -14,20 +14,18 @@ const LiveVideo: FC<Props> = ({ playbackUrl, thumbnailUrl }) => {
   }, [])
 
   return (
-    <div>
-      <div className="overflow-hidden rounded-xl">
-        <VideoPlayer
-          permanentUrl={playbackUrl}
-          hlsUrl={playbackUrl}
-          posterUrl={thumbnailUrl}
-          options={{
-            loadingSpinner: true,
-            isCurrentlyShown: true,
-            autoPlay: true
-          }}
-          isSensitiveContent={false}
-        />
-      </div>
+    <div className="overflow-hidden rounded-xl">
+      <VideoPlayer
+        permanentUrl={playbackUrl}
+        hlsUrl={playbackUrl}
+        posterUrl={thumbnailUrl}
+        options={{
+          loadingSpinner: true,
+          isCurrentlyShown: true,
+          autoPlay: true
+        }}
+        isSensitiveContent={false}
+      />
     </div>
   )
 }
--- apps/web/src/components/Stream/index.tsx
+++ apps/web/src/components/Stream/index.tsx
@@ -1,12 +1,11 @@
-import ExternalOutline from '@components/Common/Icons/ExternalOutline'
+import Alert from '@components/Common/Alert'
 import MetaTags from '@components/Common/MetaTags'
 import { VideoDetailShimmer } from '@components/Shimmers/VideoDetailShimmer'
+import { Button } from '@components/UIElements/Button'
 import SuggestedVideos from '@components/Watch/SuggestedVideos'
-import { getShortHandTime } from '@lib/formatTime'
 import { t } from '@lingui/macro'
 import { useQuery } from '@tanstack/react-query'
-import { Analytics, TRACK } from '@tape.xyz/browser'
-import { STATIC_ASSETS, WORKER_LIVE_URL } from '@tape.xyz/constants'
+import { WORKER_LIVE_URL } from '@tape.xyz/constants'
 import axios from 'axios'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -14,6 +13,7 @@ import React from 'react'
 import Custom404 from 'src/pages/404'
 
 import LiveVideo from './LiveVideo'
+import Meta from './Meta'
 
 const StreamDetails = () => {
   const {
@@ -43,7 +43,7 @@ const StreamDetails = () => {
     return <Custom404 />
   }
 
-  const { thumbnailUrl, playbackUrl, name, updatedAt, description } = liveItem
+  const { thumbnailUrl, playbackUrl, isLive } = liveItem
 
   return (
     <>
@@ -52,44 +52,20 @@ const StreamDetails = () => {
         <div className="grid grid-cols-1 gap-y-4 md:gap-4 xl:grid-cols-4">
           <div className="col-span-3 space-y-3.5">
             <LiveVideo thumbnailUrl={thumbnailUrl} playbackUrl={playbackUrl} />
-            <div className="py-1">
-              <div className="flex items-center justify-between space-x-2.5">
-                <div className="w-3/4">
-                  <div className="flex w-full min-w-0 items-start justify-between space-x-1.5">
-                    <div className="ultrawide:break-all line-clamp-2 break-words text-xl font-semibold">
-                      {name}
-                    </div>
-                  </div>
-                  <p className="line-clamp-1">{description}</p>
-                </div>
-                <div className="flex items-center overflow-hidden pt-6 opacity-70">
-                  <span>{channel}</span>
-                  <span className="middot" />
-                  {updatedAt && (
-                    <span className="whitespace-nowrap">
-                      {getShortHandTime(updatedAt)}
-                    </span>
-                  )}
-                  <span className="middot" />
-                  <Link
-                    onClick={() =>
-                      Analytics.track(TRACK.OPEN_ACTIONS.OPEN_IN_UNLONELY)
-                    }
-                    href={`https://www.unlonely.app/channels/${channel}`}
-                    target="_blank"
-                    className="flex items-center space-x-1 font-medium hover:text-indigo-500"
-                  >
-                    <img
-                      src={`${STATIC_ASSETS}/images/unlonely.png`}
-                      alt=""
-                      className="h-5 w-5 rounded-lg"
-                    />
-                    <span>Unlonely</span>
-                    <ExternalOutline className="h-3 w-3" />
+            <Meta live={liveItem} channel={channel} />
+            {!isLive && (
+              <Alert>
+                <div className="flex w-full items-center justify-between">
+                  <span className="font-semibold">
+                    Oops! This stream has ended. But don't worry, there's plenty
+                    more to explore!
+                  </span>
+                  <Link href="/">
+                    <Button size="sm">Go Home</Button>
                   </Link>
                 </div>
-              </div>
-            </div>
+              </Alert>
+            )}
           </div>
           <div className="col-span-1">
             <SuggestedVideos />
