5a4878d00a4f818feff66d8fca685ce37c7d95bd
sasi.eth <me@sasi.codes>
2023-10-07 11:54:48 +0530

    fix: mention matcher

--- packages/constants/regex.ts
+++ packages/constants/regex.ts
@@ -1,4 +1,5 @@
 export const COMMON_REGEX = {
   ZORA: /https:\/\/(?:testnet\.)?zora\.co\/collect\/(eth|oeth|base|zora|gor|ogor|basegor|zgor):(0x[\dA-Fa-f]{40})((?:\/(\d+))?|$)/,
-  URL: /\b(http|https):\/\/([\p{L}\p{N}_-]+(?:(?:\.[\p{L}\p{N}_-]+)+))([\p{L}\p{N}_.,@?^=%&:\/~+#-]*[\p{L}\p{N}_@?^=%&\/~+#-])/gu
+  URL: /\b(http|https):\/\/([\p{L}\p{N}_-]+(?:(?:\.[\p{L}\p{N}_-]+)+))([\p{L}\p{N}_.,@?^=%&:\/~+#-]*[\p{L}\p{N}_@?^=%&\/~+#-])/gu,
+  MENTION_MATCHER_REGEX: /(@lensprotocol)|(@[a-zA-Z0-9-_.]+(\.lens|\.test))/
 }
