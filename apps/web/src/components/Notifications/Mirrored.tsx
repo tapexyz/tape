c606cd9e40a25ec034048052ba749f4ef17542ad
sasi.eth <hey@sasi.codes>
2023-10-18 11:16:57 +0530

    update Mirrored.tsx

--- apps/web/src/components/Notifications/Mirrored.tsx
+++ apps/web/src/components/Notifications/Mirrored.tsx
@@ -1,5 +1,5 @@
 import HoverableProfile from '@components/Common/HoverableProfile'
-import FollowOutline from '@components/Common/Icons/FollowOutline'
+import MirrorOutline from '@components/Common/Icons/MirrorOutline'
 import { getProfile, getProfilePicture } from '@tape.xyz/generic'
 import type { MirrorNotification, ProfileMirrorResult } from '@tape.xyz/lens'
 import Link from 'next/link'
@@ -14,7 +14,7 @@ const Mirrored: FC<Props> = ({ notification: { mirrors, publication } }) => {
   return (
     <span className="flex space-x-4">
       <div className="p-1.5">
-        <FollowOutline className="h-5 w-5" />
+        <MirrorOutline className="h-5 w-5" />
       </div>
       <div>
         <span className="flex cursor-pointer -space-x-1.5">
