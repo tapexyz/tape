1ec592b0269236d3285acd4e124f3c960ffa1eee
Sasi <hey@sasi.codes>
2023-10-21 16:21:05 +0530

    chore: bump deps (#1088)
    * chore: bump deps

--- packages/constants/general.ts
+++ packages/constants/general.ts
@@ -79,7 +79,7 @@ export const POLYGON_CHAIN_ID = IS_MAINNET ? 137 : 80001
 
 // ipfs
 export const IPFS_FREE_UPLOAD_LIMIT = IS_MAINNET ? 10000 : 100 // in MB
-export const IPFS_GATEWAY_URL = 'https://ipfs.tape.xyz/ipfs'
+export const IPFS_GATEWAY_URL = 'https://gateway.4everland.link/ipfs'
 export const EVER_ENDPOINT = 'https://endpoint.4everland.co'
 export const EVER_REGION = 'us-west-2'
 
