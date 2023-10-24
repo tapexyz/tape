5d05c00d41b967d40be9768458f646d7fc5f8c79
sasi.eth <hey@sasi.codes>
2023-10-24 18:39:37 +0530

    update events.ts and tower.ts

--- packages/helpers/generic/tower.ts
+++ packages/helpers/generic/tower.ts
@@ -1,8 +1,4 @@
-import {
-  IS_MAINNET,
-  IS_PRODUCTION,
-  WORKER_TOWER_URL
-} from '@tape.xyz/constants'
+import { WORKER_TOWER_URL } from '@tape.xyz/constants'
 
 let worker: Worker
 
@@ -12,30 +8,30 @@ if (typeof Worker !== 'undefined') {
 
 export const Tower = {
   track: (name: string, properties?: Record<string, unknown>) => {
-    if (IS_MAINNET && IS_PRODUCTION) {
-      const user = JSON.parse(
-        localStorage.getItem('tape.auth.store') ||
-          JSON.stringify({ state: { selectedSimpleProfile: { id: null } } })
-      )
-      const { referrer } = document
-      const referrerDomain = referrer ? new URL(referrer).hostname : null
+    // if (IS_MAINNET && IS_PRODUCTION) {
+    const user = JSON.parse(
+      localStorage.getItem('tape.auth.store') ||
+        JSON.stringify({ state: { selectedSimpleProfile: { id: null } } })
+    )
+    const { referrer } = document
+    const referrerDomain = referrer ? new URL(referrer).hostname : null
 
-      worker.postMessage({
-        name,
-        properties,
-        actor: user.state.selectedSimpleProfile.id,
-        referrer: referrerDomain,
-        url: location.href,
-        platform: 'web'
-      })
+    worker.postMessage({
+      name,
+      properties,
+      actor: user.state?.selectedSimpleProfile?.id,
+      referrer: referrerDomain,
+      url: location.href,
+      platform: 'web'
+    })
 
-      worker.onmessage = function (event: MessageEvent) {
-        const response = event.data
-        const xhr = new XMLHttpRequest()
-        xhr.open('POST', `${WORKER_TOWER_URL}/ingest`)
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(JSON.stringify(response))
-      }
+    worker.onmessage = function (event: MessageEvent) {
+      const response = event.data
+      const xhr = new XMLHttpRequest()
+      xhr.open('POST', `${WORKER_TOWER_URL}/ingest`)
+      xhr.setRequestHeader('Content-Type', 'application/json')
+      xhr.send(JSON.stringify(response))
     }
+    // }
   }
 }
