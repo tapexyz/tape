39e28e8821309f6f40af8ff39ff54a87488eaf6c
sasi.eth <hey@sasi.codes>
2023-10-30 15:15:38 +0530

    update tower.ts

--- packages/helpers/generic/tower.ts
+++ packages/helpers/generic/tower.ts
@@ -1,8 +1,4 @@
-import {
-  IS_MAINNET,
-  IS_PRODUCTION,
-  WORKER_TOWER_URL
-} from '@tape.xyz/constants'
+import { WORKER_TOWER_URL } from '@tape.xyz/constants'
 
 import { parseJwt } from './functions/parseJwt'
 
@@ -14,31 +10,31 @@ if (typeof Worker !== 'undefined') {
 
 export const Tower = {
   track: (name: string, properties?: Record<string, unknown>) => {
-    if (IS_MAINNET && IS_PRODUCTION) {
-      const token = JSON.parse(
-        localStorage.getItem('tape.auth.store') ||
-          JSON.stringify({ state: { accessToken: '' } })
-      )
-      const actorId = parseJwt(token)?.id
-      const { referrer } = document
-      const referrerDomain = referrer ? new URL(referrer).hostname : null
+    // if (IS_MAINNET && IS_PRODUCTION) {
+    const tokenState = JSON.parse(
+      localStorage.getItem('tape.auth.store') ||
+        JSON.stringify({ state: { accessToken: '' } })
+    )
+    const actorId = parseJwt(tokenState.state.accessToken)?.id
+    const { referrer } = document
+    const referrerDomain = referrer ? new URL(referrer).hostname : null
 
-      worker.postMessage({
-        name,
-        properties,
-        actor: actorId,
-        referrer: referrerDomain,
-        url: location.href,
-        platform: 'web'
-      })
+    worker.postMessage({
+      name,
+      properties,
+      actor: actorId,
+      referrer: referrerDomain,
+      url: location.href,
+      platform: 'web'
+    })
 
-      worker.onmessage = function (event: MessageEvent) {
-        const response = event.data
-        const xhr = new XMLHttpRequest()
-        xhr.open('POST', `${WORKER_TOWER_URL}/ingest`)
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(JSON.stringify(response))
-      }
+    worker.onmessage = function (event: MessageEvent) {
+      const response = event.data
+      const xhr = new XMLHttpRequest()
+      xhr.open('POST', `${WORKER_TOWER_URL}/ingest`)
+      xhr.setRequestHeader('Content-Type', 'application/json')
+      xhr.send(JSON.stringify(response))
     }
+    // }
   }
 }
