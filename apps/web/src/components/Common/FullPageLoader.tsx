290a1920dba283001d2f89c91a1ca975cee37fac
sasi.eth <hey@sasi.codes>
2023-10-30 15:32:01 +0530

    update FullPageLoader.tsx

--- apps/web/src/components/Common/FullPageLoader.tsx
+++ apps/web/src/components/Common/FullPageLoader.tsx
@@ -8,14 +8,12 @@ const FullPageLoader = () => {
   return (
     <div className="grid h-screen place-items-center">
       <MetaTags />
-      <div className="animate-bounce">
-        <img
-          src={imageCdn(`${STATIC_ASSETS}/brand/logo.svg`)}
-          draggable={false}
-          className="h-12 w-12 md:h-16 md:w-16"
-          alt={TAPE_APP_NAME}
-        />
-      </div>
+      <img
+        src={imageCdn(`${STATIC_ASSETS}/brand/logo.svg`)}
+        draggable={false}
+        className="h-12 w-12 md:h-16 md:w-16"
+        alt={TAPE_APP_NAME}
+      />
     </div>
   )
 }
