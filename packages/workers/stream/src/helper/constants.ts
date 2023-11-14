b86dccf77113b4e614698fa99b232a154f1363b9
Sasi <hey@sasi.codes>
2023-11-14 12:17:54 +0530

    chore: cleanup

--- packages/workers/stream/src/helper/constants.ts
+++ packages/workers/stream/src/helper/constants.ts
@@ -13,20 +13,20 @@ export const CURRENT_STREAMS = [
     streamer: 'lens/letsraave',
     channel: 1,
     isLive: true
-  },
-  {
-    title: 'Rad',
-    content: 'Rad is a live stream of curated content from the internet.',
-    posterUrl:
-      'https://gw.ipfs-lens.dev/ipfs/bafybeigjzxth756zl32nfrb3nbjyqeb2tne2hdqj5aqhjz3l47o2zzivhu',
-    playbackUrl:
-      'https://gw.ipfs-lens.dev/ipfs/bafybeiem5h5h2fj56ip7qbbmbay5qqiqp7qd6av6jpex2gc3jxu6ai4yv4',
-    liveUrl:
-      'https://gw.ipfs-lens.dev/ipfs/bafybeiem5h5h2fj56ip7qbbmbay5qqiqp7qd6av6jpex2gc3jxu6ai4yv4',
-    uid: '23de4f5a-5c0a-4c0a-8b0a-5c0a4c0a8b0a',
-    pid: '0x01-0x01',
-    streamer: 'lens/streameth',
-    channel: 2,
-    isLive: true
   }
+  // {
+  //   title: 'Rad',
+  //   content: 'Rad is a live stream of curated content from the internet.',
+  //   posterUrl:
+  //     'https://gw.ipfs-lens.dev/ipfs/bafybeigjzxth756zl32nfrb3nbjyqeb2tne2hdqj5aqhjz3l47o2zzivhu',
+  //   playbackUrl:
+  //     'https://gw.ipfs-lens.dev/ipfs/bafybeiem5h5h2fj56ip7qbbmbay5qqiqp7qd6av6jpex2gc3jxu6ai4yv4',
+  //   liveUrl:
+  //     'https://gw.ipfs-lens.dev/ipfs/bafybeiem5h5h2fj56ip7qbbmbay5qqiqp7qd6av6jpex2gc3jxu6ai4yv4',
+  //   uid: '23de4f5a-5c0a-4c0a-8b0a-5c0a4c0a8b0a',
+  //   pid: '0x01-0x01',
+  //   streamer: 'lens/streameth',
+  //   channel: 2,
+  //   isLive: true
+  // }
 ]
