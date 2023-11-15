4900fa47c978581d4e07b1a23b413432dd27f023
Sasi <hey@sasi.codes>
2023-11-15 22:01:12 +0530

    Update playbackUrl in CURRENT_STREAMS constant

--- packages/workers/stream/src/helper/constants.ts
+++ packages/workers/stream/src/helper/constants.ts
@@ -4,7 +4,8 @@ export const CURRENT_STREAMS = [
     content:
       'Dance with Ronnie from anywhere in the globe featuring Veronika Fleyta, GALLiVANTER, Henri Bergmann, Jamiie and Jimi Jules. \n Livestream at 5PM UTC: https://rad.lol/stream',
     posterUrl: 'https://static.tape.xyz/images/fallback-thumbnail.webp',
-    playbackUrl: 'https://livepeercdn.studio/hls/4cb59lo3lqedybxf/index.m3u8',
+    playbackUrl:
+      'https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/04820al76z49vlik/index.m3u8',
     liveUrl: 'https://livepeercdn.studio/hls/4cb59lo3lqedybxf/index.m3u8',
     uid: '23de4f5a-5c0a-4c0a-8b0a-5c0a4c0a8b01',
     pid: '0x04-0x47-DA-2dd07e63',
