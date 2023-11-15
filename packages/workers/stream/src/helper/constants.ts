4748bd698c4dc9b4e0ac5b437a67c56cb2cc6713
Sasi <hey@sasi.codes>
2023-11-15 18:40:14 +0530

    Update current streams with rAAVE Istanbul 2023 live stream.

--- packages/workers/stream/src/helper/constants.ts
+++ packages/workers/stream/src/helper/constants.ts
@@ -1,7 +1,8 @@
 export const CURRENT_STREAMS = [
   {
-    title: "Let's rAAVE",
-    content: 'Hosting decentralized parties since 2021.',
+    title: 'rAAVE İstanbul 2023',
+    content:
+      'Live streaming till 4 AM with Veronika Fleyta, GALLiVANTER, Henri Bergmann b2b Jamiie and Jimi Jules.',
     posterUrl: 'https://static.tape.xyz/images/fallback-thumbnail.webp',
     playbackUrl:
       'https://gw.ipfs-lens.dev/ipfs/bafybeia6adey4ywft3ta5nz6nf73krvcj5reckyjvpxo6ugt6znbcuk7dy',
@@ -11,18 +12,6 @@ export const CURRENT_STREAMS = [
     pid: '0x01-0x01',
     streamer: 'lens/letsraave',
     channel: 1,
-    isLive: true
-  },
-  {
-    title: 'Rad',
-    content: 'Rad is a live stream of curated content from the internet.',
-    posterUrl: 'https://static.tape.xyz/images/fallback-thumbnail.webp',
-    playbackUrl: 'https://livepeercdn.studio/hls/ac8dk2zj53sdnybu/index.m3u8',
-    liveUrl: 'https://livepeercdn.studio/hls/ac8dk2zj53sdnybu/index.m3u8',
-    uid: '23de4f5a-5c0a-4c0a-8b0a-5c0a4c0a8b0a',
-    pid: '0x98e0-0x0a-DA-91406d88',
-    streamer: 'lens/pablov',
-    channel: 2,
     isLive: true
   }
 ]
