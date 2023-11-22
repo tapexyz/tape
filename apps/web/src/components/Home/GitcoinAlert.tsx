7a4306eba7f23e147f05cb2152c5515637eabd57
salti <justsoantsy@gmail.com>
2023-11-22 16:40:20 -0700

    Adding the other Gitcoin Round

--- apps/web/src/components/Home/GitcoinAlert.tsx
+++ apps/web/src/components/Home/GitcoinAlert.tsx
@@ -28,7 +28,10 @@ const GitcoinAlert = () => {
         </p>
         <Flex>
           <Link href="/gitcoin" target="_blank">
-            <Button highContrast>Contribute</Button>
+            <Button highContrast>Contribute Community Round</Button>
+          </Link>
+          <Link href="https://explorer.gitcoin.co/#/round/10/0xc34745b3852df32d5958be88df2bee0a83474001/0xc34745b3852df32d5958be88df2bee0a83474001-15" target="_blank">
+            <Button highContrast>Contribute Social Round</Button>
           </Link>
         </Flex>
       </div>
