0a7551c0eeb8e7f4510606f070a1a777f03c3e2c
salti <justsoantsy@gmail.com>
2023-11-30 21:21:49 -0700

    reverting back to what has worked best

--- apps/web/src/components/Common/Search/GlobalSearch.tsx
+++ apps/web/src/components/Common/Search/GlobalSearch.tsx
@@ -117,8 +117,7 @@ const GlobalSearch = () => {
 
   const Content = () => (
     <>
-      <div className="relative z-20 md:absolute md:inset-x-0 md:top-full md:mt-2"></div>
-      <TextField.Root className="laptop:w-[800px] dark:bg-brand-850 w-[500px] rounded-full bg-white">
+      <TextField.Root className="laptop:w-[800px] dark:bg-brand-850 bg-brand-50 z-20 w-[215px] rounded-full md:w-[500px]">
         <TextField.Slot px="3">
           <SearchOutline className="h-4 w-4" />
           <span className="sr-only">Search</span>
@@ -136,7 +135,7 @@ const GlobalSearch = () => {
       </TextField.Root>
       <div
         className={clsx(
-          'rounded-medium tape-border dark:bg-brand-850 z-10 mt-1 w-full bg-white text-base shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none md:absolute md:top-[calc(100%+10px)]',
+          'rounded-medium tape-border dark:bg-brand-850 z-10 mt-1 w-[300px] bg-white text-base shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none md:absolute md:w-[500px]',
           { hidden: debouncedValue.length === 0 }
         )}
       >
