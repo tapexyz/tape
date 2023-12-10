93955016f71fce45c1477f1597781223f5d29477
Sasi <hey@sasi.codes>
2023-12-10 13:01:37 +0530

    Refactor getShouldUploadVideo function to check if video is older than 1 day and not optimized

--- packages/helpers/generic/functions/getShouldUploadVideo.ts
+++ packages/helpers/generic/functions/getShouldUploadVideo.ts
@@ -2,12 +2,12 @@ import type { PrimaryPublication, VideoMetadataV3 } from '@tape.xyz/lens'
 
 export const getShouldUploadVideo = (video: PrimaryPublication): boolean => {
   const metadata = video.metadata as VideoMetadataV3
-  // check if video is older than 1 week
+  // check if video is older than 1 day and not optimized
   const now = new Date()
-  const oneWeek = 1000 * 60 * 60 * 24 * 7
-  const oneWeekAgo = new Date(now.getTime() - oneWeek)
+  const oneDay = 1000 * 60 * 60 * 24
+  const oneDayAgo = new Date(now.getTime() - oneDay)
   const createdAt = new Date(video.createdAt)
-  if (createdAt > oneWeekAgo && !Boolean(metadata.asset.video.optimized?.uri)) {
+  if (createdAt > oneDayAgo && !Boolean(metadata.asset.video.optimized?.uri)) {
     return true
   }
   return false
