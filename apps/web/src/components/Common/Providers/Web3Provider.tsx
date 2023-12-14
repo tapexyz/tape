5366e5fa484b8bf0cc4168fe6ffd04203f04ad9b
Sasi <hey@sasi.codes>
2023-12-14 18:54:43 +0530

    chore: add qrModalOptions to WalletConnectConnector

--- apps/web/src/components/Common/Providers/Web3Provider.tsx
+++ apps/web/src/components/Common/Providers/Web3Provider.tsx
@@ -39,7 +39,10 @@ const connectors: any = [
   new CoinbaseWalletConnector({ options: { appName: TAPE_APP_NAME } }),
   new WalletConnectConnector({
     options: {
-      projectId: WC_PROJECT_ID
+      projectId: WC_PROJECT_ID,
+      qrModalOptions: {
+        explorerExcludedWalletIds: 'ALL'
+      }
     },
     chains
   })
