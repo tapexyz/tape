04be61ee49a54971c0197dbf224624b4ece41ea0
Sasi <hey@sasi.codes>
2023-12-20 13:26:24 +0530

    chore: update dependencies

--- packages/constants/verified/avara.ts
+++ packages/constants/verified/avara.ts
@@ -1,7 +1,8 @@
 import { IS_MAINNET } from '../general'
 
-export const AAVE_MEMBERS = IS_MAINNET
+export const AVARA_MEMBERS = IS_MAINNET
   ? [
+      '0x01cddb', // build
       '0x01a709', // avara
       '0x5c95', // 2irl4u
       '0x04', // letsraave
--- packages/constants/verified/index.ts
+++ packages/constants/verified/index.ts
@@ -1,7 +1,11 @@
 import { IS_MAINNET } from '../general'
-import { AAVE_MEMBERS } from './aave-members'
+import { AVARA_MEMBERS } from './avara'
 import { CREATORS } from './creators'
 
 export const CORE_MEMBERS = IS_MAINNET ? ['0x2d'] : ['0x0256']
 
-export const VERIFIED_CHANNELS = [...CREATORS, ...AAVE_MEMBERS, ...CORE_MEMBERS]
+export const VERIFIED_CHANNELS = [
+  ...CREATORS,
+  ...AVARA_MEMBERS,
+  ...CORE_MEMBERS
+]
