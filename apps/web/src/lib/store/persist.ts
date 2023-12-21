2481417302845fc4c7c96f9ac53a5c49059a195f
Sasi <hey@sasi.codes>
2023-12-21 17:57:13 +0530

    feat: use cookies (#1190)

--- apps/web/src/lib/store/persist.ts
+++ apps/web/src/lib/store/persist.ts
@@ -37,7 +37,6 @@ export const usePersistStore = create(
       queuedVideos: [],
       setQueuedVideos: (queuedVideos) => set({ queuedVideos }),
       setQueuedComments: (queuedComments) => set({ queuedComments }),
-
       selectedNotificationsFilter:
         CustomNotificationsFilterEnum.ALL_NOTIFICATIONS,
       setSelectedNotificationsFilter: (selectedNotificationsFilter) =>
