3cb9ffe7109c36663e9e8e7dc30c8c3b9d83f924
Sasi <hey@sasi.codes>
2023-12-26 14:03:01 +0530

    fix: lint

--- packages/constants/algorithms.ts
+++ packages/constants/algorithms.ts
@@ -2,7 +2,6 @@ import { ALGO_PROVIDER, AlgoType } from '@tape.xyz/lens/custom-types'
 
 export const FEED_ALGORITHMS = [
   {
-    provider: ALGO_PROVIDER.K3L,
     algorithms: [
       {
         name: 'Karma Crowdsourced',
@@ -16,6 +15,7 @@ export const FEED_ALGORITHMS = [
         name: 'Karma Recommended',
         strategy: AlgoType.K3L_RECOMMENDED
       }
-    ]
+    ],
+    provider: ALGO_PROVIDER.K3L
   }
 ]
--- packages/constants/feature-flags.ts
+++ packages/constants/feature-flags.ts
@@ -2,29 +2,29 @@ import { IS_MAINNET } from './general'
 import { CORE_MEMBERS } from './verified'
 
 export enum FEATURE_FLAGS {
+  BANGERS = 'Bangers',
   POST_WITH_SOURCE_URL = 'PostWithSource',
-  PROFILE_NFTS = 'ProfileNfts',
-  BANGERS = 'Bangers'
+  PROFILE_NFTS = 'ProfileNfts'
 }
 
 type FeatureFlag = {
-  flag: string
   enabledFor: string[]
+  flag: string
 }
 
 export const featureFlags: FeatureFlag[] = [
   {
-    flag: FEATURE_FLAGS.POST_WITH_SOURCE_URL,
-    enabledFor: CORE_MEMBERS
+    enabledFor: CORE_MEMBERS,
+    flag: FEATURE_FLAGS.POST_WITH_SOURCE_URL
   },
   {
-    flag: FEATURE_FLAGS.PROFILE_NFTS,
-    enabledFor: CORE_MEMBERS
+    enabledFor: CORE_MEMBERS,
+    flag: FEATURE_FLAGS.PROFILE_NFTS
   },
   {
-    flag: FEATURE_FLAGS.BANGERS,
     enabledFor: IS_MAINNET
       ? ['0x5c95', '0x0d', '0x6ba0', ...CORE_MEMBERS]
-      : CORE_MEMBERS
+      : CORE_MEMBERS,
+    flag: FEATURE_FLAGS.BANGERS
   }
 ]
