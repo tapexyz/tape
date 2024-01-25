5c48cbc3d780182767392b6c6e91ab4ba34fa69c
Sasi <hey@sasi.codes>
2024-01-25 18:37:08 +0530

    fix: connectors

--- apps/web/src/components/Login/Connectors.tsx
+++ apps/web/src/components/Login/Connectors.tsx
@@ -1,6 +1,6 @@
 import useProfileStore from '@lib/store/idb/profile'
 import { Button, Callout, CheckOutline, WarningOutline } from '@tape.xyz/ui'
-import React, { memo } from 'react'
+import React, { memo, useMemo } from 'react'
 import type { Connector } from 'wagmi'
 import { useAccount, useConnect } from 'wagmi'
 
@@ -11,10 +11,7 @@ const Connectors = () => {
 
   const { connector: connected } = useAccount()
   const { connectors, connectAsync, isPending, error } = useConnect()
-
-  if (activeProfile?.id) {
-    return <Authenticate />
-  }
+  console.log('🚀 ~ Connectors ~ connectors:', connectors)
 
   const onChooseConnector = async (connector: Connector) => {
     try {
@@ -31,10 +28,22 @@ const Connectors = () => {
     }
   }
 
+  const filteredConnectors = useMemo(() => {
+    return connectors
+      .filter((connector) => connector.id !== 'injected')
+      .sort(
+        (a, b) => Number(b.type === 'injected') - Number(a.type === 'injected')
+      )
+  }, [connectors])
+
+  if (activeProfile?.id) {
+    return <Authenticate />
+  }
+
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-col gap-2">
-        {connectors.map((c) => (
+        {filteredConnectors.map((c) => (
           <Button
             key={c.id}
             size="md"
