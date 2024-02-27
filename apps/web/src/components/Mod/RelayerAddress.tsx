2d8ec8db9f6a963a7843ea138368d2c74183f6d0
Sasi <hey@sasi.codes>
2024-02-28 00:04:56 +0530

    Add useReadContract hook to RelayerAddress component

--- apps/web/src/components/Mod/RelayerAddress.tsx
+++ apps/web/src/components/Mod/RelayerAddress.tsx
@@ -1,7 +1,9 @@
+import { TAPE_SIGNUP_PROXY_ABI } from '@tape.xyz/abis'
+import { TAPE_SIGNUP_PROXY_ADDRESS } from '@tape.xyz/constants'
 import { type FC } from 'react'
 import type { Address } from 'viem'
 import { formatUnits } from 'viem'
-import { useBalance } from 'wagmi'
+import { useBalance, useReadContract } from 'wagmi'
 
 interface RelayerAddressProps {
   address: Address
@@ -13,11 +15,18 @@ const RelayerAddress: FC<RelayerAddressProps> = ({ address }) => {
     query: { refetchInterval: 5000 }
   })
 
+  const { data: allowed } = useReadContract({
+    abi: TAPE_SIGNUP_PROXY_ABI,
+    address: TAPE_SIGNUP_PROXY_ADDRESS,
+    args: [address],
+    functionName: 'allowedRelayerAddresses'
+  })
+
   const balance = data ? parseFloat(formatUnits(data.value, 18)) : 0
 
   return (
     <div className="mb-1 flex justify-between rounded-xl border px-5 py-4">
-      {address}
+      {address} {allowed ? '✅' : '❌'}
       <div>
         <b>{balance} </b>MATIC
       </div>
