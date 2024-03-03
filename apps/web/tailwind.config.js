1e5985f3b336780fd112f1f76bc9cc31c6b9c8ed
Sasi <hey@sasi.codes>
2024-03-03 12:18:34 +0530

    Remove important flag from tailwind.config.js

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,7 +3,6 @@ const base = require('@tape.xyz/ui/tailwind-preset')
 /** @type {import ('tailwindcss').Config} */
 module.exports = {
   ...base,
-  important: true,
   content: ['./src/**/*.{js,ts,jsx,tsx}', '../../packages/**/*.{ts,tsx}'],
   plugins: [
     require('@tailwindcss/aspect-ratio'),
