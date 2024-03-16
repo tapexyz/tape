baa946d5b38416ec2023d896408cbd7e0a026490
William Lopez-Cordero <williamlopezc@gmail.com>
2024-03-16 19:03:56 -0400

    (Image): fix imageCdn

--- packages/helpers/generic/functions/imageCdn.ts
+++ packages/helpers/generic/functions/imageCdn.ts
@@ -1,6 +1,5 @@
 import {
   IMAGE_TRANSFORMATIONS,
-  IS_PRODUCTION,
   LENS_IMAGEKIT_SNAPSHOT_URL
 } from '@dragverse/constants'
 
@@ -12,18 +11,14 @@ export const imageCdn = (
     return url
   }
 
-  return type && IS_PRODUCTION
-    ? `${LENS_IMAGEKIT_SNAPSHOT_URL}/${IMAGE_TRANSFORMATIONS[type]}/${url}`
-    : url
+  if (url.includes(LENS_IMAGEKIT_SNAPSHOT_URL)) {
+    const splitedUrl = url.split('/')
+    const path = splitedUrl[splitedUrl.length - 1]
 
-  // if (url.includes(LENS_IMAGEKIT_SNAPSHOT_URL)) {
-  //   const splitedUrl = url.split('/')
-  //   const path = splitedUrl[splitedUrl.length - 1]
+    return type
+      ? `${LENS_IMAGEKIT_SNAPSHOT_URL}/${IMAGE_TRANSFORMATIONS[type]}/${path}`
+      : url
+  }
 
-  //   return type
-  //     ? `${LENS_IMAGEKIT_SNAPSHOT_URL}/${IMAGE_TRANSFORMATIONS[type]}/${path}`
-  //     : url
-  // }
-
-  // return url
+  return url
 }
