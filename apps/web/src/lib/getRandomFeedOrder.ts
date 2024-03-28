60133ad65c512f03b47d34a7ede8545d6979f68f
Sasi <hey@sasi.codes>
2024-03-28 22:25:38 +0530

    chore: update feed order

--- apps/web/src/lib/getRandomFeedOrder.ts
+++ apps/web/src/lib/getRandomFeedOrder.ts
@@ -2,7 +2,6 @@ import { ExplorePublicationsOrderByType } from '@tape.xyz/lens'
 
 export const getRandomFeedOrder = () => {
   const orders = [
-    ExplorePublicationsOrderByType.TopMirrored,
     ExplorePublicationsOrderByType.TopCommented,
     ExplorePublicationsOrderByType.TopReacted,
     ExplorePublicationsOrderByType.TopQuoted,
