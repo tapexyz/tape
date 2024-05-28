d658ded70f10d0d9276d7a9e68a32c831edf7cdb
Sasi <hey@sasi.codes>
2024-05-28 11:43:07 +0530

    chore: update dependencies

--- packages/constants/general.ts
+++ packages/constants/general.ts
@@ -16,7 +16,7 @@ export const TAPE_WEBSITE_URL = IS_MAINNET
 export const FALLBACK_THUMBNAIL_URL = `${STATIC_ASSETS}/images/fallback-thumbnail.webp`
 export const FALLBACK_COVER_URL = `${STATIC_ASSETS}/images/fallback-cover.svg`
 export const OG_IMAGE = `${STATIC_ASSETS}/brand/og.png`
-export const LENS_IMAGEKIT_SNAPSHOT_URL = 'https://ik.imagekit.io/lt'
+export const LENS_IMAGEKIT_SNAPSHOT_URL = 'https://ik.imagekit.io/lenstubemain'
 export const TAPE_CURATOR_ID = '0x04298d'
 
 // infinite scroll
