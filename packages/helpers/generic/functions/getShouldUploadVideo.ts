13af48e5404c62e4d05d7ba2fc3500aa96e4f3e6
Sasi <hey@sasi.codes>
2024-06-28 14:04:37 +0530

    fix: player upload

--- packages/helpers/generic/functions/getShouldUploadVideo.ts
+++ packages/helpers/generic/functions/getShouldUploadVideo.ts
@@ -2,16 +2,13 @@ import type { PrimaryPublication, VideoMetadataV3 } from '@tape.xyz/lens'
 
 export const getShouldUploadVideo = (video: PrimaryPublication): boolean => {
   const metadata = video.metadata as VideoMetadataV3
-  // check if video is older than 1 day and not optimized
-  const now = new Date()
-  const oneDay = 1000 * 60 * 60 * 24
-  const oneDayAgo = new Date(now.getTime() - oneDay)
+  // Define the time threshold for video optimization (4 hours ago)
+  const fourHoursAgo = new Date(Date.now() - 4 * 60 * 60 * 1000)
   const createdAt = new Date(video.createdAt)
 
-  const uri = metadata.asset.video.optimized?.uri ?? ''
+  // Check if the video's URI is not optimized (not ending with .m3u8)
+  const isNotOptimized = !metadata.asset.video.optimized?.uri?.endsWith('.m3u8')
 
-  if (createdAt < oneDayAgo && (!Boolean(uri) || !uri.includes('.m3u8'))) {
-    return true
-  }
-  return false
+  // Return true if the video is older than 4 hours and not optimized
+  return createdAt < fourHoursAgo && isNotOptimized
 }
