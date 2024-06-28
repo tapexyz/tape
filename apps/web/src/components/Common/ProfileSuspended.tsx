e6eeec26ba6a516b936ff58e1fc09cd7fb208cf0
Sasi <hey@sasi.codes>
2024-06-28 13:13:48 +0530

    feat: suspend violations

---
+++ apps/web/src/components/Common/ProfileSuspended.tsx
@@ -0,0 +1,19 @@
+import MetaTags from '@components/Common/MetaTags'
+import { Button } from '@tape.xyz/ui'
+import Link from 'next/link'
+import React from 'react'
+
+const ProfileSuspended = () => {
+  return (
+    <div className="flex h-[calc(100vh-8rem)] flex-col items-center justify-center space-y-4 text-center">
+      <MetaTags title="Suspended" />
+      <h1 className="text-4xl font-bold">Suspended</h1>
+      <div className="mb-6">Author profile has been suspended.</div>
+      <Link href="/">
+        <Button>Go Home</Button>
+      </Link>
+    </div>
+  )
+}
+
+export default ProfileSuspended
