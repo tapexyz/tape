1f84ba848317c43b4731e5b5545344998ddb2d72
Sasi <hey@sasi.codes>
2024-07-05 20:16:55 +0530

    chore: remove unused code

--- apps/web/src/components/Mod/Signup.tsx
+++ apps/web/src/components/Mod/Signup.tsx
@@ -13,17 +13,8 @@ import { useGenerateLensApiRelayAddressQuery } from '@tape.xyz/lens'
 import { Button, Input } from '@tape.xyz/ui'
 import React, { useState } from 'react'
 import toast from 'react-hot-toast'
-import type { Address } from 'viem'
 import { formatUnits } from 'viem'
 import { useBalance, useReadContract, useWriteContract } from 'wagmi'
-
-import RelayerAddress from './RelayerAddress'
-
-const RELAYER_ADDRESSES = [
-  '0xc4407B6dBC56a6cF06d9B6E637Ae12bf13481a38',
-  '0xA8B6A140b53412E5F66a4a0944534059864F9A3f',
-  '0x684bf2D496ecd6D1194cf510E2B623721c4A12e5'
-]
 
 const Signup = () => {
   const [loading, setLoading] = useState(false)
@@ -140,12 +131,6 @@ const Signup = () => {
             </Button>
           </div>
         </div>
-      </div>
-      <div>
-        <h2 className="mb-2 font-bold">Relayers</h2>
-        {RELAYER_ADDRESSES.map((address) => (
-          <RelayerAddress key={address} address={address as Address} />
-        ))}
       </div>
       <div>
         <h2 className="mb-2 font-bold">Mint for user</h2>
