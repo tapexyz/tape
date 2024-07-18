a9051ae926864e553f9212a4f2e86e676cf9eec6
Sasi <hey@sasi.codes>
2024-07-18 21:00:53 +0530

    feat: add validation logic on canUploadedToIpfs

--- packages/helpers/generic/functions/canUploadedToIpfs.ts
+++ packages/helpers/generic/functions/canUploadedToIpfs.ts
@@ -2,26 +2,43 @@ import {
   IPFS_FREE_UPLOAD_LIMIT,
   IPFS_FREE_UPLOAD_MAX_LIMIT
 } from '@tape.xyz/constants'
-import type { Profile } from '@tape.xyz/lens'
+import type { Profile, ProfileOnchainIdentity } from '@tape.xyz/lens'
+
+const validateOnchainIdentity = (onchainIdentity: ProfileOnchainIdentity) => {
+  const { proofOfHumanity, ens, sybilDotOrg, worldcoin } = onchainIdentity
+  // Validation logic
+  return (
+    proofOfHumanity || ens?.name || sybilDotOrg.verified || worldcoin.isHuman
+  )
+}
 
 export const canUploadedToIpfs = (
   bytes: number,
   activeProfile: Profile | null
 ) => {
-  const sponsored = activeProfile?.sponsor
-  const hasFollowers = (activeProfile?.stats.followers ?? 0) > 0 ? true : false
-  const hasGoodScore =
-    (activeProfile?.stats.lensClassifierScore ?? 0) > 10 ? true : false
-  const allowed = hasGoodScore && sponsored && hasFollowers
-  if (bytes === null || bytes === undefined || !allowed) {
+  if (!activeProfile || bytes === null || bytes === undefined) {
     return false
   }
-  const megaBytes = bytes ? bytes / 1024 ** 2 : 0
 
-  const canUploadMaxLimit =
-    (activeProfile?.stats.lensClassifierScore ?? 0) > 0.6 * 10000 ? true : false
+  const score = activeProfile.stats.lensClassifierScore ?? 0
+  const hasOnchainIdentity = validateOnchainIdentity(
+    activeProfile.onchainIdentity
+  )
+
+  // Return true if the user has an onchain identity or a score greater than 100 (0 to 10000 range)
+  if (hasOnchainIdentity || score > 100) {
+    return true
+  }
+
+  // Calculate the size of the file in megabytes
+  const megaBytes = bytes / 1024 ** 2
+
+  // Determine the upload limit based on the user's score
+  const canUploadMaxLimit = score > 0.6 * 10000 // 60%
   const limit = canUploadMaxLimit
     ? IPFS_FREE_UPLOAD_MAX_LIMIT
     : IPFS_FREE_UPLOAD_LIMIT
-  return bytes ? (megaBytes < limit ? true : false) : false
+
+  // Check if the file size is within the allowed limit
+  return megaBytes < limit
 }
