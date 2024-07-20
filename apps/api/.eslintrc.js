d7e518f632214694ba76d7a9834d692bb47ea2f8
Sasi <hey@sasi.codes>
2024-07-20 21:25:01 +0530

    chore: update tsconfig.json to enable esModuleInterop and set noEmit to false

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,3 +1,4 @@
 module.exports = {
-  extends: [require.resolve('@tape.xyz/config/eslint/index.js')]
+  extends: [require.resolve('@tape.xyz/config/eslint/index.js')],
+  ignorePatterns: ['dist/']
 }
