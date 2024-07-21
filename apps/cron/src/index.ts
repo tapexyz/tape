4e063b474c2f76c947a797aff51c20c23b51407e
Sasi <hey@sasi.codes>
2024-07-22 00:08:05 +0530

    chore: Update cron schedule to run flushEvents every 4 hours

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -4,8 +4,13 @@ import cron from 'node-cron'
 
 import { flushEvents } from './services/events'
 
-// Schedule the flushEvents function to run every 4 hour
-cron.schedule('* * * * *', async () => {
-  console.log('[cron] Flushing tower events', new Date())
-  await flushEvents()
-})
+const run = () => {
+  // Schedule the flushEvents function to run every 4 hour
+  cron.schedule('* * * * *', async () => {
+    // cron.schedule('0 */4 * * *', async () => {
+    console.log('[cron] Flushing tower events', new Date())
+    await flushEvents()
+  })
+}
+
+run()
