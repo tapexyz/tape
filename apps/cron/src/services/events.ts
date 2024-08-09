e65e411d96b410179680a140573d2754fa6cee02
Sasi <hey@sasi.codes>
2024-08-09 14:34:54 +0530

    chore: add log

--- apps/cron/src/services/events.ts
+++ apps/cron/src/services/events.ts
@@ -9,7 +9,7 @@ const flushEvents = async (): Promise<void> => {
     const length = await rLength(QUEUE_KEY)
     // Loop as batch of BATCH_SIZE events
     for (let i = 0; i < length; i += BATCH_SIZE) {
-      const startTime = Date.now()
+      const startTime = performance.now()
 
       // pick BATCH_SIZE events from the start of the queue (for eg 0 to 4999)
       const rawEvents = await rLoad(QUEUE_KEY, i, BATCH_SIZE - 1)
@@ -26,10 +26,9 @@ const flushEvents = async (): Promise<void> => {
         await rTrim(QUEUE_KEY, events.length)
       }
 
-      const endTime = Date.now()
-      const timeTaken = endTime - startTime
+      const took = performance.now() - startTime
       console.log(
-        `[cron] tower events - batch inserted ${events.length} events to clickhouse in ${timeTaken}ms`
+        `[cron] tower events - batch inserted ${events.length} events to clickhouse in ${took}ms`
       )
     }
   } catch (error) {
