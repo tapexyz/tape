83144034917062ac4edc6ce8be9124d67f95d90e
Sasi <hey@sasi.codes>
2024-08-12 14:10:09 +0530

    feat: backup clickhouse to s3

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 import cron from 'node-cron'
 
+import { backupEventsToS3 } from './services/backup/events'
 import { flushEvents } from './services/events'
 import { vacuumPostgres } from './services/vacuum'
 import { wake } from './services/wake'
@@ -22,4 +23,10 @@ cron.schedule('0 */4 * * *', async () => {
 cron.schedule('0 0 * * 0', async () => {
   console.log('[cron] Vacuuming postgres', new Date())
   await vacuumPostgres()
+})
+
+// Schedule the backupEventsToS3 function to run every 5 hour
+cron.schedule('0 */5 * * *', async () => {
+  console.log('[cron] Backing up events to S3', new Date())
+  await backupEventsToS3()
 })
---
+++ apps/cron/src/services/backup/events.ts
@@ -0,0 +1,71 @@
+import { clickhouseClient, REDIS_EXPIRY, rGet, rSet } from '@tape.xyz/server'
+
+const backupEventsToS3 = async () => {
+  try {
+    const cacheKey = 'backups:events:offset'
+    const batchSize = 100000
+
+    // Get the last offset from Redis (or start from 0 if no offset is stored)
+    let offset = parseInt((await rGet(cacheKey)) || '0', 10)
+
+    // Calculate the range for the current batch
+    const startRange = offset
+    const endRange = offset + batchSize
+
+    const fileName = `events-${startRange}-${endRange}.csv`
+
+    // Check the number of rows in the current batch
+    const rowsCountResult = await clickhouseClient.query({
+      format: 'JSONEachRow',
+      query: `
+        SELECT count(*) as count
+        FROM (
+            SELECT *
+            FROM events
+            ORDER BY created
+            LIMIT ${batchSize} OFFSET ${offset}
+        ) as subquery;
+      `
+    })
+
+    const rowsCount = await rowsCountResult.json<{ count: string }>()
+
+    if (parseInt(rowsCount[0].count) === batchSize) {
+      // Proceed with the backup if there are rows to back up
+      await clickhouseClient.query({
+        format: 'JSONEachRow',
+        query: `
+          INSERT INTO FUNCTION
+          s3(
+            'https://tape-db-backups.s3.us-east-1.amazonaws.com/tape-clickhouse-backups/${fileName}',
+            '${process.env.S3_ACCESS_KEY_ID}',
+            '${process.env.S3_SECRET_ACCESS_KEY}',
+            'CSV'
+          )
+          SETTINGS s3_truncate_on_insert=1
+          SELECT * FROM events
+          ORDER BY created
+          LIMIT ${batchSize} OFFSET ${offset};
+        `
+      })
+
+      // Increment the offset
+      offset += batchSize
+      await rSet(cacheKey, offset.toString(), REDIS_EXPIRY.FOREVER)
+
+      console.info(
+        `[Cron] Backup completed successfully for ${fileName} with offset ${offset}`
+      )
+    } else {
+      const remainingEvents = batchSize - parseInt(rowsCount[0].count)
+
+      console.info(
+        `[Cron] No more events to back up at offset ${offset}. ${remainingEvents} events still need to be backed up.`
+      )
+    }
+  } catch (error) {
+    console.error('[Cron] backupEventsToS3 - Error processing events', error)
+  }
+}
+
+export { backupEventsToS3 }
