afe15cdd0e93b759d4fccdd53b24d1b9757f2de4
Sasi <hey@sasi.codes>
2024-08-12 17:13:22 +0530

    chore: bump dependencies (#1456)

---
+++ apps/webv3/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  experimental: {
+    typedRoutes: true
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  }
+}
+
+export default nextConfig
---
+++ apps/webv3/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      gm
+    </main>
+  )
+}
