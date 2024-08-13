e02f3a9ce3f4c454607ee9c5c0c1b21e716db773
Sasi <hey@sasi.codes>
2024-08-13 11:00:23 +0530

    chore: wake clickhouse

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -5,21 +5,24 @@ import cron from 'node-cron'
 import { backupEventsToS3 } from './services/backup/events'
 import { flushEvents } from './services/events'
 import { vacuumPostgres } from './services/vacuum'
+import { wakeClickHouse } from './services/wake'
 
 // Schedule the flushEvents function to run every 4 hour
 cron.schedule('0 */4 * * *', async () => {
   console.log('[cron] Flushing tower events', new Date())
+  await wakeClickHouse()
   await flushEvents()
+})
+
+// Schedule the backupEventsToS3 function to run every midnight
+cron.schedule('0 0 * * *', async () => {
+  console.log('[cron] Backing up events to S3', new Date())
+  await wakeClickHouse()
+  await backupEventsToS3()
 })
 
 // Schedule the vacuumPostgres function to run every sunday at midnight
 cron.schedule('0 0 * * 0', async () => {
   console.log('[cron] Vacuuming postgres', new Date())
   await vacuumPostgres()
-})
-
-// Schedule the backupEventsToS3 function to run every 4 hour
-cron.schedule('0 */4 * * *', async () => {
-  console.log('[cron] Backing up events to S3', new Date())
-  await backupEventsToS3()
 })
---
+++ apps/cron/src/services/wake.ts
@@ -0,0 +1,31 @@
+import { clickhouseClient } from '@tape.xyz/server'
+
+const wakeClickHouse = async () => {
+  try {
+    let attempt = 0
+    let maxAttempts = 5
+    let delay = 2000 // 2 seconds delay between attempts
+
+    while (attempt < maxAttempts) {
+      const result = await clickhouseClient.ping()
+      if (result.success) {
+        console.log('[wake] ClickHouse database is awake!')
+        return
+      } else {
+        console.log(
+          `[wake] Attempt ${attempt + 1} failed, retrying in ${delay / 1000} seconds...`
+        )
+        // Wait for a short period of time before retrying
+        await new Promise((resolve) => setTimeout(resolve, delay))
+        attempt++
+      }
+    }
+    console.error(
+      '[wake] Failed to wake up ClickHouse database after multiple attempts.'
+    )
+  } catch (error) {
+    console.error('[wake] Error waking up ClickHouse database:', error)
+  }
+}
+
+export { wakeClickHouse }
