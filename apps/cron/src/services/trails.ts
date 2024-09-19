739d168faffb43070f19a8107b618853239050cf
Sasi <hey@sasi.codes>
2024-09-19 18:36:39 +0530

    chore: update batch

--- apps/cron/src/services/trails.ts
+++ apps/cron/src/services/trails.ts
@@ -2,7 +2,7 @@ import { REDIS_KEYS } from "@tape.xyz/constants";
 import { clickhouseClient, rLength, rLoad, rTrim } from "@tape.xyz/server";
 
 const QUEUE_KEY = REDIS_KEYS.TRAILS;
-const BATCH_SIZE = 500;
+const BATCH_SIZE = 1000;
 
 const flushTrails = async (): Promise<void> => {
   try {
@@ -11,7 +11,7 @@ const flushTrails = async (): Promise<void> => {
     for (let i = 0; i < length; i += BATCH_SIZE) {
       const startTime = performance.now();
 
-      // pick BATCH_SIZE events from the start of the queue (for eg 0 to 499)
+      // pick BATCH_SIZE events from the start of the queue (for eg 0 to 999)
       const rawEvents = await rLoad(QUEUE_KEY, i, BATCH_SIZE - 1);
       const trails: Record<string, string>[] = rawEvents.map((trail) =>
         JSON.parse(trail)
