58fd6e17e230fafb5dcd5233db598172056b8b29
Sasi <hey@sasi.codes>
2024-09-20 10:39:21 +0530

    chore: format routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -21,25 +21,23 @@ import verified from "./routes/verified";
 
 const app = new Hono();
 
-app.use(logger());
-app.use(originLogger);
-app.use("*", cors);
+app.use(logger()).use(originLogger).use("*", cors);
 
-app.get("/", (c) => c.text("nothing to see here, visit tape.xyz"));
-
-app.route("/did", did);
-app.route("/sts", sts);
-app.route("/tail", tail);
-app.route("/tower", tower);
-app.route("/oembed", oembed);
-app.route("/trails", trails);
-app.route("/avatar", avatar);
-app.route("/gateway", gateway);
-app.route("/curated", curated);
-app.route("/toggles", toggles);
-app.route("/metadata", metadata);
-app.route("/verified", verified);
-app.route("/allowed-tokens", allowedTokens);
+app
+  .get("/api", (c) => c.text("nothing to see here, visit tape.xyz"))
+  .route("/did", did)
+  .route("/sts", sts)
+  .route("/tail", tail)
+  .route("/tower", tower)
+  .route("/oembed", oembed)
+  .route("/trails", trails)
+  .route("/avatar", avatar)
+  .route("/gateway", gateway)
+  .route("/curated", curated)
+  .route("/toggles", toggles)
+  .route("/metadata", metadata)
+  .route("/verified", verified)
+  .route("/allowed-tokens", allowedTokens);
 
 serve(app, (info) => {
   console.log(`API listening on http://localhost:${info.port}`);
