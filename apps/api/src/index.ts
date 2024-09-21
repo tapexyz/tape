fc56f3f277c870d1633c3ef006e699caec1bca9b
Sasi <hey@sasi.codes>
2024-09-21 11:15:45 +0530

    chore: cleanup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -25,9 +25,6 @@ app.use(logger()).use(originLogger).use("*", cors);
 
 app
   .get("/", (c) => c.text("nothing to see here, visit tape.xyz"))
-  .get("/robots.txt", (c) =>
-    c.text("User-agent: *\nDisallow: /\nAllow: /oembed")
-  )
   .route("/did", did)
   .route("/sts", sts)
   .route("/tail", tail)
