0ebb4e15d471de20905c6e0cad419ca51f080659
Sasi <hey@sasi.codes>
2024-09-23 10:53:55 +0530

    chore: update constants

--- apps/web/src/components/Common/ProfileSuspended.tsx
+++ apps/web/src/components/Common/ProfileSuspended.tsx
@@ -6,9 +6,12 @@ import MetaTags from "@/components/Common/MetaTags";
 const ProfileSuspended = () => {
   return (
     <div className="flex h-[calc(100vh-8rem)] flex-col items-center justify-center space-y-4 text-center">
-      <MetaTags title="Suspended" />
-      <h1 className="font-bold text-4xl">Suspended</h1>
-      <div className="mb-6">Author profile has been suspended.</div>
+      <MetaTags title="Not Found" />
+      <h1 className="font-bold text-4xl">Not Found</h1>
+      <div className="mb-6">
+        Author profile has been masked. If you believe this is an error, please
+        contact us.
+      </div>
       <Link href="/">
         <Button>Go Home</Button>
       </Link>
--- packages/constants/suspended.ts
+++ packages/constants/suspended.ts
@@ -2,19 +2,16 @@ import { IS_MAINNET } from "./general";
 
 export const SUSPENDED_PROFILES = IS_MAINNET
   ? [
+      "0x084311",
       "0x03eee1",
       "0x6e64",
       "0x79f2",
       "0x4e24",
-      "0x79f2",
       "0x923a",
       "0x99",
       "0x9b5d",
       "0x9cb4",
-      "0x9cb4",
       "0x8441",
-      "0x9b5d",
-      "0x0dc2",
-      "0x9cb4"
+      "0x0dc2"
     ]
   : [];
