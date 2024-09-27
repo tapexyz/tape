10e20230a65699df19780770e13acfacd37174c1
Sasi <hey@sasi.codes>
2024-09-27 13:30:42 +0530

    chore: cleanup

--- apps/webv3/src/app/mod/page.tsx
+++ apps/webv3/src/app/mod/page.tsx
@@ -1,15 +1,13 @@
 import { rqClient } from "@/providers/react-query";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { Posts } from "./_components/posts";
-import { modExplorePublicationsQuery } from "./queries";
 
 export default function ModPage() {
-  void rqClient.prefetchInfiniteQuery(modExplorePublicationsQuery);
+  // void rqClient.prefetchInfiniteQuery(modExplorePublicationsQuery);
 
   return (
     <div className="flex min-h-screen flex-col items-center 2xl:container">
       <HydrationBoundary state={dehydrate(rqClient)}>
-        <Posts />
+        {/* <Posts /> */}
       </HydrationBoundary>
     </div>
   );
