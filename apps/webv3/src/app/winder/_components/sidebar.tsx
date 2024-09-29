4aa3ca6b3aed42ae310347d634193385e5ea078c
Sasi <hey@sasi.codes>
2024-09-29 12:16:25 +0530

    fix: re render

--- apps/webv3/src/app/winder/_components/sidebar.tsx
+++ apps/webv3/src/app/winder/_components/sidebar.tsx
@@ -5,36 +5,42 @@ import Link from "next/link";
 import { useState } from "react";
 import { components } from ".";
 
-export const Sidebar = () => {
+const Li = ({ item }: { item: { id: string; label: string } }) => {
+  const { id, label } = item;
   const [activeTab, setActiveTab] = useState("");
 
+  return (
+    <li
+      onFocus={() => setActiveTab(id)}
+      onMouseOver={() => setActiveTab(id)}
+      onMouseLeave={() => setActiveTab("")}
+    >
+      <Link
+        className="-mx-3 relative flex rounded-custom px-3 py-2 text-secondary/70 capitalize no-underline outline-none"
+        href={`#${id}`}
+        prefetch={false}
+      >
+        {activeTab === id ? (
+          <motion.span
+            key={activeTab}
+            layoutId="winder-sidebar-link-indicator"
+            transition={{ duration: 0.2, bounce: 0, type: "spring" }}
+            className="absolute inset-0 rounded-custom bg-secondary"
+          />
+        ) : null}
+        <span className="relative">{label}</span>
+      </Link>
+    </li>
+  );
+};
+
+export const Sidebar = () => {
   return (
     <aside className="no-scrollbar sticky top-[100px] bottom-0 z-10 hidden h-[calc(100vh-100px)] overflow-y-auto border-custom px-6 py-3 md:block md:border-x">
       <h1 className="py-5 font-bold text-xs uppercase">Components</h1>
       <ul className="w-full">
         {components.map(({ id, label }) => (
-          <li
-            key={id}
-            onFocus={() => setActiveTab(id)}
-            onMouseOver={() => setActiveTab(id)}
-            onMouseLeave={() => setActiveTab("")}
-          >
-            <Link
-              className="-mx-3 relative flex rounded-custom px-3 py-2 text-secondary/70 capitalize no-underline outline-none"
-              href={`#${id}`}
-              prefetch={false}
-            >
-              {activeTab === id ? (
-                <motion.span
-                  key={activeTab}
-                  layoutId="link-indicator"
-                  transition={{ duration: 0.2, bounce: 0, type: "spring" }}
-                  className="absolute inset-0 rounded-custom bg-secondary"
-                />
-              ) : null}
-              <span className="relative">{label}</span>
-            </Link>
-          </li>
+          <Li key={id} item={{ id, label }} />
         ))}
       </ul>
     </aside>
