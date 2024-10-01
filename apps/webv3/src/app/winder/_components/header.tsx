126593595760306e62a933f847334e26155ec974
Sasi <hey@sasi.codes>
2024-10-01 22:01:58 +0530

    chore: cleanup components

--- apps/webv3/src/app/winder/_components/header.tsx
+++ apps/webv3/src/app/winder/_components/header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <nav className="sticky top-0 z-10 flex h-[100px] items-center justify-between border-custom border-b bg-site/60 p-6 backdrop-blur-xl md:border-x">
       <Link href="/winder" prefetch={false}>
-        <h1 className="font-semibold font-serif text-2xl">tape</h1>
+        <h1 className="font-semibold font-serif text-xl leading-5">Winder</h1>
         <span className="text-muted text-sm">brand & design system</span>
       </Link>
       <ThemeSwitcherExpanded id="winder-header" />
--- apps/webv3/src/app/winder/_components/intro.tsx
+++ apps/webv3/src/app/winder/_components/intro.tsx
@@ -1,30 +1,7 @@
 import { motion } from "framer-motion";
 import { IntroSvg } from "./intro-svg";
 
-const AnimatedText = ({ char, index }: { char: string; index: number }) => (
-  <motion.span
-    key={char}
-    className="inline-block"
-    drag
-    dragSnapToOrigin
-    dragMomentum={false}
-    dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
-    dragTransition={{
-      bounceDamping: 20,
-      bounceStiffness: 600
-    }}
-    initial={{ opacity: 0, translateY: "50%", rotateX: -45 }}
-    animate={{ opacity: 1, translateY: 0, rotateX: 0 }}
-    transition={{
-      type: "spring",
-      bounce: 0,
-      duration: 0.6,
-      delay: index * 0.03
-    }}
-  >
-    {char}
-  </motion.span>
-);
+const title = "Winder";
 
 export const IntroSection = () => {
   return (
@@ -35,22 +12,31 @@ export const IntroSection = () => {
 
       <div className="relative mx-[40%]">
         <span className="truncate font-bold font-serif text-3xl lg:text-5xl">
-          {"Winder".split("").map((char, index) => (
-            <AnimatedText key={char} index={index} char={char} />
+          {title.split("").map((char, index) => (
+            <motion.span
+              key={char}
+              className="inline-block"
+              drag
+              dragSnapToOrigin
+              dragMomentum={false}
+              dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+              dragTransition={{
+                bounceDamping: 20,
+                bounceStiffness: 600
+              }}
+              initial={{ opacity: 0, translateY: "50%", rotateX: -45 }}
+              animate={{ opacity: 1, translateY: 0, rotateX: 0 }}
+              transition={{
+                type: "spring",
+                bounce: 0,
+                duration: 0.6,
+                delay: index * 0.03
+              }}
+            >
+              {char}
+            </motion.span>
           ))}
         </span>
-        <div className="text-center text-muted text-sm">
-          {"design system".split("").map((char, index) => {
-            const sanitizedChar = char === " " ? "\u00A0" : char;
-            return (
-              <AnimatedText
-                index={index}
-                key={sanitizedChar}
-                char={sanitizedChar}
-              />
-            );
-          })}
-        </div>
       </div>
     </section>
   );
