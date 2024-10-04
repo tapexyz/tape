572927f239634ba75b90662f4960dcd5c680ef08
Sasi <hey@sasi.codes>
2024-10-04 18:39:53 +0530

    chore: add log

--- apps/api/src/routes/tower.ts
+++ apps/api/src/routes/tower.ts
@@ -44,7 +44,7 @@ const processEvent = async (req: HonoRequest, event: RequestInput) => {
   }
 
   const ip = req.header("cf-connecting-ip");
-  console.log("🚀 ~ processEvent ~ req:", req);
+  console.log("🚀 ~ processEvent ~ req:", req.raw.headers);
   const user_agent = req.header("user-agent");
 
   const parser = new UAParser(user_agent || "");
