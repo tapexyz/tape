e398047c3ff133bbd88684f64368293fccc685ec
Sasi <hey@sasi.codes>
2024-10-10 17:44:59 +0530

    chore: cleanup

--- apps/webv3/src/app/winder/_components/sidebar.tsx
+++ apps/webv3/src/app/winder/_components/sidebar.tsx
@@ -1,47 +1,31 @@
 "use client";
 
-import { motion } from "framer-motion";
 import Link from "next/link";
-import { type Dispatch, type SetStateAction, useState } from "react";
 import { base, components } from "./map";
 
 type LiProps = {
   item: { id: string; label: string };
-  hoverTab: string;
-  setHoverTab: Dispatch<SetStateAction<string>>;
 };
 
-const Li = ({ item, hoverTab, setHoverTab }: LiProps) => {
+const Li = ({ item }: LiProps) => {
   const { id, label } = item;
 
   return (
-    <li
-      onFocus={() => setHoverTab(id)}
-      onMouseOver={() => setHoverTab(id)}
-      onMouseLeave={() => setHoverTab("")}
-    >
+    <li>
       <Link
-        className="-mx-3 relative flex rounded-custom px-3 py-2 text-primary/70 capitalize"
+        className="-mx-3 group relative flex rounded-custom px-3 py-2 text-primary/70 capitalize transition duration-300"
         href={`#${id}`}
         prefetch={false}
       >
-        {hoverTab === id ? (
-          <motion.span
-            key={hoverTab}
-            layoutId="winder-sidebar-link-indicator"
-            transition={{ duration: 0.2, bounce: 0, type: "spring" }}
-            className="absolute inset-0 rounded-custom bg-secondary"
-          />
-        ) : null}
-        <span className="relative">{label}</span>
+        <span className="bg-[length:0%_2px] bg-gradient-to-r bg-left-bottom from-primary bg-no-repeat transition-all duration-200 ease-out group-hover:bg-[length:100%_2px] group-focus:bg-[length:100%_2px]">
+          {label}
+        </span>
       </Link>
     </li>
   );
 };
 
 export const Sidebar = () => {
-  const [hoverTab, setHoverTab] = useState("");
-
   return (
     <aside className="no-scrollbar sticky top-[100px] bottom-0 z-10 hidden h-[calc(100vh-100px)] overflow-y-auto border-custom px-6 py-4 md:block md:border-x">
       <h1 className="select-none py-5 font-bold text-xs uppercase">
@@ -49,12 +33,7 @@ export const Sidebar = () => {
       </h1>
       <ul className="w-full">
         {base.map((item) => (
-          <Li
-            key={item.id}
-            item={item}
-            hoverTab={hoverTab}
-            setHoverTab={setHoverTab}
-          />
+          <Li key={item.id} item={item} />
         ))}
       </ul>
       <h1 className="select-none py-5 font-bold text-xs uppercase">
@@ -62,12 +41,7 @@ export const Sidebar = () => {
       </h1>
       <ul className="w-full">
         {components.map((item) => (
-          <Li
-            key={item.id}
-            item={item}
-            hoverTab={hoverTab}
-            setHoverTab={setHoverTab}
-          />
+          <Li key={item.id} item={item} />
         ))}
       </ul>
     </aside>
