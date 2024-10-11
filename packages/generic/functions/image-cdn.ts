9b6d959c95e97b282a97519255cc7eeecf5a0f75
Sasi <hey@sasi.codes>
2024-10-11 13:34:05 +0530

    chore: remove unused code

--- packages/generic/functions/image-cdn.ts
+++ packages/generic/functions/image-cdn.ts
@@ -15,15 +15,4 @@ export const imageCdn = (
   return type && IS_PRODUCTION
     ? `${LENS_IMAGEKIT_SNAPSHOT_URL}/${IMAGE_TRANSFORMATIONS[type]}/${url}`
     : url;
-
-  // if (url.includes(LENS_IMAGEKIT_SNAPSHOT_URL)) {
-  //   const splitedUrl = url.split('/')
-  //   const path = splitedUrl[splitedUrl.length - 1]
-
-  //   return type
-  //     ? `${LENS_IMAGEKIT_SNAPSHOT_URL}/${IMAGE_TRANSFORMATIONS[type]}/${path}`
-  //     : url
-  // }
-
-  // return url
 };
