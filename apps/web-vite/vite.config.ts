532feea0c6a7c2c3a1c6b50de1b18d8e8fd630b6
Sasi <hey@sasi.codes>
2024-10-12 13:08:42 +0530

    chore: update tsconfig

--- apps/web-vite/vite.config.ts
+++ apps/web-vite/vite.config.ts
@@ -1,18 +1,9 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import biomePlugin from "vite-plugin-biome";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    biomePlugin({
-      mode: "check",
-      applyFixes: true,
-      failOnError: true
-    }),
-    react()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
