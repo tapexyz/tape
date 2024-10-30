dc71849afd0ea5a8fc39fe1c3a24cdfcc3c71bf6
Sasi <hey@sasi.codes>
2024-10-30 20:37:17 +0530

    chore: cleanup components

--- apps/web-vite/src/components/home/invite.tsx
+++ apps/web-vite/src/components/home/invite.tsx
@@ -1,13 +1,38 @@
 import { Button } from "@tape.xyz/winder";
+import { m, useMotionValue, useTransform } from "framer-motion";
 import { memo } from "react";
 
 export const Invite = memo(() => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const backgroundX = useTransform(mouseX, [0, 1], [-3, 3]);
+  const backgroundY = useTransform(mouseY, [0, 1], [-3, 3]);
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    const { currentTarget, clientX, clientY } = event;
+    const { left, top, width, height } = currentTarget.getBoundingClientRect();
+
+    const xPos = (clientX - left) / width;
+    const yPos = (clientY - top) / height;
+
+    mouseX.set(xPos);
+    mouseY.set(yPos);
+  };
+
   return (
     <div
-      style={{ backgroundImage: "url(/images/hero.webp)" }}
-      className="flex h-96 flex-col justify-end overflow-hidden rounded-card bg-center bg-cover bg-theme bg-no-repeat grayscale transition-filter duration-500 hover:grayscale-0"
+      onMouseMove={handleMouseMove}
+      className="relative flex h-96 flex-col justify-end overflow-hidden rounded-card bg-theme"
     >
-      <div className="flex flex-wrap items-end justify-between gap-4 bg-gradient-to-t from-black/70 p-5">
+      <m.div
+        style={{
+          x: backgroundX,
+          y: backgroundY
+        }}
+        className="-inset-5 absolute bg-[url(/images/hero.webp)] bg-center bg-cover bg-no-repeat grayscale transition-filter duration-500 hover:grayscale-0"
+      />
+      <div className="relative flex flex-wrap items-end justify-between gap-4 bg-gradient-to-t from-black/70 p-5">
         <h2 className="max-w-xl text-[28px] text-white leading-[26px] tracking-tighter">
           Join the movement towards a more transparent, secure, and user-centric
           social experience while expanding your network.
