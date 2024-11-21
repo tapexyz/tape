db4e60bda443c14caa9422da27754632f38da365
Sasi <hey@sasi.codes>
2024-11-21 13:56:03 +0530

    chore: cleanup

--- packages/constants/endpoints.ts
+++ packages/constants/endpoints.ts
@@ -1,5 +1,5 @@
 export enum LensEndpoint {
   Mainnet = "https://api-v2.lens.dev",
   Staging = "https://api-amoy.lens-v2.crtlkey.com",
-  Testnet = "https://api-v2-amoy.lens.dev"
+  Testnet = "https://api.testnet.lens.dev"
 }
